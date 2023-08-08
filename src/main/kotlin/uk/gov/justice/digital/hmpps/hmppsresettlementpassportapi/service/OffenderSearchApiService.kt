11e5203f29b5f6c70e3c26c1b66f3979e31ac8f9
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-08-08 11:36:20 +0100

    RP-409 Feature improved (#33)
    * Update WebClientConfiguration.kt
    
    * RP-409 Feature
    
    * RP-409 Feature, pathway status included

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/OffenderSearchApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/OffenderSearchApiService.kt
@@ -5,14 +5,19 @@ import kotlinx.coroutines.flow.flow
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.PathwayStatus
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.Prisoners
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.PrisonersList
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonersapi.PrisonerRequest
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonersapi.PrisonersSearch
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonersapi.PrisonersSearchList
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.PathwayRepository
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 @Service
 class OffenderSearchApiService(
+  private val pathwayRepository: PathwayRepository,
   private val offendersSearchWebClientClientCredentials: WebClient,
 ) {
 
@@ -67,8 +72,9 @@ class OffenderSearchApiService(
    * returning a complete list.
    * Requires role PRISONER_IN_PRISON_SEARCH or PRISONER_SEARCH
    */
-  suspend fun getPrisonersByPrisonId(dateRangeAPI: Boolean, prisonId: String, days: Long, pageNumber: Int, pageSize: Int, sort: String): PrisonersSearchList {
+  suspend fun getPrisonersByPrisonId(dateRangeAPI: Boolean, prisonId: String, days: Long, pageNumber: Int, pageSize: Int, sort: String): PrisonersList {
     val offenders = mutableListOf<PrisonersSearch>()
+    // val prisoners = mutableListOf<Prisoners>()
     if (dateRangeAPI) {
       val pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd")
       val earliestReleaseDate = LocalDate.now().minusDays(days).format(pattern)
@@ -104,11 +110,39 @@ class OffenderSearchApiService(
     val endIndex = (pageNumber * pageSize) + (pageSize)
     if (startIndex < endIndex && endIndex <= offenders.size) {
       val searchList = offenders.subList(startIndex, endIndex)
-      return PrisonersSearchList(searchList, searchList.toList().size, pageNumber, sort, offenders.size, (endIndex == offenders.size))
+      val pList: List<Prisoners> = objectMapper(searchList)
+      return PrisonersList(pList, pList.toList().size, pageNumber, sort, offenders.size, (endIndex == pList.size))
     } else if (startIndex < endIndex) {
       val searchList = offenders.subList(startIndex, offenders.size)
-      return PrisonersSearchList(searchList, searchList.toList().size, pageNumber, sort, offenders.size, true)
+      val pList: List<Prisoners> = objectMapper(searchList)
+      return PrisonersList(pList, pList.toList().size, pageNumber, sort, offenders.size, true)
     }
-    return PrisonersSearchList(null, null, null, null, 0, false)
+    return PrisonersList(null, null, null, null, 0, false)
+  }
+
+  private fun objectMapper(searchList: List<PrisonersSearch>): List<Prisoners> {
+    val prisonersList = mutableListOf<Prisoners>()
+    val pathwayRepoData = pathwayRepository.findAll()
+    searchList.forEach {
+      val prisoner = Prisoners(
+        it.prisonerNumber,
+        it.firstName,
+        it.middleNames,
+        it.lastName,
+        it.releaseDate,
+        it.nonDtoReleaseDateType,
+      )
+      val argStatus = ArrayList<PathwayStatus>()
+
+      pathwayRepoData.forEach {
+        if (it.active) {
+          val pathwayStatus = PathwayStatus(it.name, "Not Started")
+          argStatus.add(pathwayStatus)
+        }
+      }
+      prisoner.status = argStatus
+      prisonersList.add(prisoner)
+    }
+    return prisonersList
   }
 }

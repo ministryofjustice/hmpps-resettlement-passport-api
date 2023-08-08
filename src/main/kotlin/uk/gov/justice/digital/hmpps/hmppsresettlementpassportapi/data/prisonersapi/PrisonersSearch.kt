dc8413d1777586a34e005f170100fce156a6885d
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-08-08 09:22:04 +0100

    RP-409 Feature added (#31)
    * RP-409 Feature added
    
    * Update WebClientConfiguration.kt

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/prisonersapi/PrisonersSearch.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonersapi
+
+import java.time.LocalDate
+
+data class PrisonersSearch(
+  val prisonerNumber: String,
+  val firstName: String,
+  val middleNames: String? = null,
+  val lastName: String,
+  val releaseDate: LocalDate? = null,
+  val nonDtoReleaseDateType: String? = null,
+  val lastUpdatedDate: LocalDate? = null,
+)
+
+data class PrisonerRequest(
+  val earliestReleaseDate: String,
+  val latestReleaseDate: String,
+  val prisonIds: List<String>,
+)
+data class PrisonersSearchList(
+  val content: List<PrisonersSearch>?,
+  val pageSize: Int?,
+  val page: Int?,
+  val sortName: String?,
+  val totalElements: Int?,
+  val last: Boolean,
+)

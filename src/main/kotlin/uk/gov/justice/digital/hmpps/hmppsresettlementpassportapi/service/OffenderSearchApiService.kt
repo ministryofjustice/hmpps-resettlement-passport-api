fcf28804818993cf3f56b5f127fc3f9076187e67
Abdul <abdul.rahuman@digital.justice.gov.uk>
2023-08-08 12:49:40 +0100

    RP-409 Validation Added

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/OffenderSearchApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/OffenderSearchApiService.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.flow
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config.NoDataWithCodeFoundException
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.PathwayStatus
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.Prisoners
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.PrisonersList
@@ -75,6 +76,11 @@ class OffenderSearchApiService(
   suspend fun getPrisonersByPrisonId(dateRangeAPI: Boolean, prisonId: String, days: Long, pageNumber: Int, pageSize: Int, sort: String): PrisonersList {
     val offenders = mutableListOf<PrisonersSearch>()
     // val prisoners = mutableListOf<Prisoners>()
+    if (pageNumber <0 || pageSize<0 )
+      throw NoDataWithCodeFoundException(
+        "Data",
+        "Page $pageNumber and Size $pageSize",
+      )
     if (dateRangeAPI) {
       val pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd")
       val earliestReleaseDate = LocalDate.now().minusDays(days).format(pattern)
@@ -95,6 +101,13 @@ class OffenderSearchApiService(
       }
     }
 
+    val startIndex = (pageNumber * pageSize)
+    if (startIndex > offenders.size)
+      throw NoDataWithCodeFoundException(
+        "Data",
+        "Page $pageNumber",
+      )
+
     when (sort) {
       "releaseDate,ASC" -> offenders.sortBy { it.releaseDate }
       "firstName,ASC" -> offenders.sortBy { it.firstName }
@@ -106,7 +119,8 @@ class OffenderSearchApiService(
       "prisonerNumber,DESC" -> offenders.sortByDescending { it.prisonerNumber }
     }
 
-    val startIndex = (pageNumber * pageSize)
+
+
     val endIndex = (pageNumber * pageSize) + (pageSize)
     if (startIndex < endIndex && endIndex <= offenders.size) {
       val searchList = offenders.subList(startIndex, endIndex)

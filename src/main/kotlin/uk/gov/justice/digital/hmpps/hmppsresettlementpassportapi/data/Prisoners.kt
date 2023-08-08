274323ccaebb188d4d333ea3b038f48a72156a6c
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-08-08 11:36:20 +0100

    RP-409 Feature improved (#33)
    * Update WebClientConfiguration.kt
    
    * RP-409 Feature
    
    * RP-409 Feature, pathway status included

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/Prisoners.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/Prisoners.kt
@@ -1,23 +1,29 @@
-package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonersapi
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data
 
 import java.time.LocalDate
 
-data class PrisonersSearch(
+data class Prisoners(
   val prisonerNumber: String,
   val firstName: String,
   val middleNames: String? = null,
   val lastName: String,
   val releaseDate: LocalDate? = null,
-  val nonDtoReleaseDateType: String? = null,
+  val releaseType: String? = null,
+  val lastUpdatedDate: LocalDate? = null,
+  var status: List<PathwayStatus>? = ArrayList(),
 )
 
+data class PathwayStatus(
+  val pathway: String? = null,
+  val status: String? = null,
+)
 data class PrisonerRequest(
   val earliestReleaseDate: String,
   val latestReleaseDate: String,
   val prisonIds: List<String>,
 )
-data class PrisonersSearchList(
-  val content: List<PrisonersSearch>?,
+data class PrisonersList(
+  val content: List<Prisoners>?,
   val pageSize: Int?,
   val page: Int?,
   val sortName: String?,

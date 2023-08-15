38d162af6499cbc2947bfd36eef1e63a90acb5ba
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-08-15 11:05:55 +0100

    RP-365 Feature Added (#43)
    * RP-365 Feature Added
    
    * RP365 Feature added
    
    * Update OffenderSearchApiMockServer.kt
    
    * Removed the unused json

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/PrisonersIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/PrisonersIntegrationTest.kt
@@ -8,9 +8,9 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   fun `Get All Prisoners happy path`() {
     val expectedOutput = File("src/test/resources/testdata/prisoners/prisoners.json").inputStream().readBytes().toString(Charsets.UTF_8)
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 200)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 200)
     webTestClient.get()
-      .uri("/resettlement-passport/prison/$prisonId/prisoners?page=0&size=10&sort=releaseDate,DESC")
+      .uri("/resettlement-passport/prison/$prisonId/prisoners?term=&page=0&size=10&sort=releaseDate,DESC")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))
       .exchange()
       .expectStatus().isOk
@@ -22,9 +22,9 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   fun `Get All Prisoners sort by releaseDate ascending happy path`() {
     val expectedOutput = File("src/test/resources/testdata/prisoners/prisoners-ascending.json").inputStream().readBytes().toString(Charsets.UTF_8)
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 200)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 200)
     webTestClient.get()
-      .uri("/resettlement-passport/prison/$prisonId/prisoners?page=0&size=10&sort=releaseDate,ASC")
+      .uri("/resettlement-passport/prison/$prisonId/prisoners?term=&page=0&size=10&sort=releaseDate,ASC")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))
       .exchange()
       .expectStatus().isOk
@@ -45,7 +45,7 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   @Test
   fun `Get All Prisoners  Internal Error`() {
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 500)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 500)
     webTestClient.get()
       .uri("/resettlement-passport/prison/$prisonId/prisoners")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))
@@ -59,7 +59,7 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   @Test
   fun `Get All Prisoners  negative Page number`() {
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 404)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 404)
     webTestClient.get()
       .uri("/resettlement-passport/prison/$prisonId/prisoners?page=-1&size=10&sort=releaseDate,ASC")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))
@@ -74,7 +74,7 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   @Test
   fun `Get All Prisoners  negative Page size`() {
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 404)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 404)
     webTestClient.get()
       .uri("/resettlement-passport/prison/$prisonId/prisoners?page=1&size=-1&sort=releaseDate,ASC")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))
@@ -89,7 +89,7 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   @Test
   fun `Get All Prisoners with no page and no size as Internal Error`() {
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 500)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 500)
     webTestClient.get()
       .uri("/resettlement-passport/prison/$prisonId/prisoners?sort=releaseDate,ASC")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))
@@ -103,7 +103,7 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   @Test
   fun `Get All Prisoners with no sort as Internal Error`() {
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 500)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 500)
     webTestClient.get()
       .uri("/resettlement-passport/prison/$prisonId/prisoners?page=1&sie=10")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))
@@ -117,7 +117,7 @@ class PrisonersIntegrationTest : IntegrationTestBase() {
   @Test
   fun `Get All Prisoners with sort invalid`() {
     val prisonId = "MDI"
-    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, 500, 0, 200)
+    offenderSearchApiMockServer.stubGetPrisonersList(prisonId, "", 500, 0, 200)
     webTestClient.get()
       .uri("/resettlement-passport/prison/$prisonId/prisoners?page=0&size=10&sort=xxxxx")
       .headers(setAuthorisation(roles = listOf("ROLE_ADMIN")))

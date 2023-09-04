c56ac517db5bdc2e0c77bd6657c094078ccfcb78
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-09-04 16:25:26 +0100

    RP2-474 Casenotes Sorting Feature added (#71)
    * RP2-474 Casenotes Sorting Feature added
    
    * RP2-474 Casenotes Sorting Feature added - build fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/CaseNotesIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/CaseNotesIntegrationTest.kt
@@ -11,7 +11,7 @@ class CaseNotesIntegrationTest : IntegrationTestBase() {
     caseNotesApiMockServer.stubGetCaseNotesOldList("G4274GN", 500, 0, "REPORTS", "REP_IEP", 200)
     caseNotesApiMockServer.stubGetCaseNotesNewList("G4274GN", 500, 0, 200)
     webTestClient.get()
-      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10")
+      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10&sort=occurenceDateTime,DESC")
       .headers(setAuthorisation(roles = listOf("ROLE_RESETTLEMENT_PASSPORT_EDIT")))
       .exchange()
       .expectStatus().isOk
@@ -24,7 +24,7 @@ class CaseNotesIntegrationTest : IntegrationTestBase() {
   fun `Get CaseNotes unauthorized`() {
     // Failing to set a valid Authorization header should result in 401 response
     webTestClient.get()
-      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10")
+      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10&sort=occurenceDateTime,DESC")
       .exchange()
       .expectStatus().isEqualTo(401)
   }
@@ -32,7 +32,7 @@ class CaseNotesIntegrationTest : IntegrationTestBase() {
   @Test
   fun `Get CaseNotes forbidden`() {
     webTestClient.get()
-      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10")
+      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10&sort=occurenceDateTime,DESC")
       .headers(setAuthorisation())
       .exchange()
       .expectStatus().isForbidden
@@ -43,12 +43,28 @@ class CaseNotesIntegrationTest : IntegrationTestBase() {
     caseNotesApiMockServer.stubGetCaseNotesOldList("G4274GN", 500, 0, "REPORTS", "", 500)
     caseNotesApiMockServer.stubGetCaseNotesNewList("G4274GN", 500, 0, 500)
     webTestClient.get()
-      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10")
+      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10&sort=occurenceDateTime,DESC")
       .headers(setAuthorisation(roles = listOf("ROLE_RESETTLEMENT_PASSPORT_EDIT")))
       .exchange()
       .expectStatus().isEqualTo(500)
       .expectHeader().contentType("application/json")
       .expectBody()
       .jsonPath("status").isEqualTo(500)
+  }
+
+  @Test
+  fun `Get All CaseNotes for a Prisoner  happy path sort by Pathway`() {
+    val expectedOutput = readFile("testdata/expectation/case-notes-sort-pathway.json")
+    // TODO "REPORTS" Need to be replace with "GEN" and searchSubTerm to be "RESET"
+    caseNotesApiMockServer.stubGetCaseNotesOldList("G4274GN", 500, 0, "REPORTS", "REP_IEP", 200)
+    caseNotesApiMockServer.stubGetCaseNotesNewList("G4274GN", 500, 0, 200)
+    webTestClient.get()
+      .uri("/resettlement-passport/case-notes/G4274GN?page=0&size=10&sort=pathway,ASC")
+      .headers(setAuthorisation(roles = listOf("ROLE_RESETTLEMENT_PASSPORT_EDIT")))
+      .exchange()
+      .expectStatus().isOk
+      .expectHeader().contentType("application/json")
+      .expectBody()
+      .json(expectedOutput)
   }
 }

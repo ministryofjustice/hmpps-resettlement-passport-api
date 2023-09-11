d89d7da7788d7156eb201a0a3e8e6d3d72bc7286
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-09-11 10:25:41 +0100

    RP2-501 Add key worker to staff contacts API

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/keyworkerapi/KeyWorkerDTO.kt
@@ -0,0 +1,7 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.keyworkerapi
+
+data class KeyWorkerDTO (
+  val staffId: Long?,
+  val firstName: String?,
+  val lastName: String?,
+)

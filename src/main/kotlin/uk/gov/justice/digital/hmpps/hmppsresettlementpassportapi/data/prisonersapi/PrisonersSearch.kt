4bc5566e17811b101f8cfd93ca3581358f41f262
Abdul <abdul.rahuman@digital.justice.gov.uk>
2023-09-20 09:46:23 +0100

    RP2-264 Feature Added

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/prisonersapi/PrisonersSearch.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/prisonersapi/PrisonersSearch.kt
@@ -8,12 +8,20 @@ data class PrisonersSearch(
   val middleNames: String? = null,
   val lastName: String,
   val releaseDate: LocalDate? = null,
+  val confirmedReleaseDate: LocalDate? = null,
+  val actualParoleDate: LocalDate? = null,
+  val homeDetentionCurfewActualDate: LocalDate? = null,
+  val conditionalReleaseDate: LocalDate? = null,
+  val automaticReleaseDate: LocalDate? = null,
+  val homeDetentionCurfewEligibilityDate: LocalDate? = null,
+  val paroleEligibilityDate: LocalDate? = null,
   val nonDtoReleaseDateType: String? = null,
   val dateOfBirth: LocalDate? = null,
   val youthOffender: Boolean?,
   val prisonId: String,
   val prisonName: String,
   val cellLocation: String?,
+  var displayReleaseDate: LocalDate? = null,
 )
 
 data class PrisonerRequest(

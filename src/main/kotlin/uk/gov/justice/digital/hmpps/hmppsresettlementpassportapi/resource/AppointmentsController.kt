38d5ad7c6dc153bee093a823e4961940b3ef1adb
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-10-06 16:52:52 +0100

    Bug fix for Appointments - Added default value and handling null when data not exists in the response (#137)
    * Bug RP2-629 fix - Accommodation date check for null
    
    * RP2-629 build fix
    
    * Bug fix for Appointments - Added default value and handling null when data not exists in the response
    
    * Bug fix for Appointments - build fix
    
    * Bug fix for Appointments - Added default value and handling null when data not exists in the response
    
    * Bug fix for Appointments - build fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/AppointmentsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/AppointmentsController.kt
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.AppointmentsList
@@ -58,9 +59,11 @@ class AppointmentsController(
     nomisId: String,
     @Schema(example = "0", required = true)
     @Parameter(required = true, description = "Zero-based page index (0..N)")
+    @RequestParam(value = "page", defaultValue = "0")
     page: Int,
     @Schema(example = "10", required = true)
     @Parameter(required = true, description = "The size of the page to be returned")
+    @RequestParam(value = "size", defaultValue = "10")
     size: Int,
   ): AppointmentsList = appointmentsService.getAppointmentsByNomisId(nomisId, LocalDate.now(), LocalDate.now().plusDays(365), page, size)
 }

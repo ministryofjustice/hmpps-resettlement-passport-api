715d4044e9dc6f7a005da201443340229be48dd6
Abdul <abdul.rahuman@digital.justice.gov.uk>
2023-10-06 16:32:33 +0100

    Bug fix for Appointments - build fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/AppointmentsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/AppointmentsController.kt
@@ -9,7 +9,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.AppointmentsList
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.AppointmentsApiService

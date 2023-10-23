48df0511a6a9ef040d744ca4c246e048253fdd39
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-10-23 11:45:28 +0100

    RP2-680 Metric Feature (#150)
    * RP2-680 Metric Feature
    
    * RP-680 Feature Metric added build fix
    
    * RP2-680 Metric Feature code review comment fix

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/MetricsService.kt
@@ -0,0 +1,71 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.external.OffenderSearchApiService
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.external.PrisonRegisterApiService
+import java.time.LocalDate
+
+@Service
+class MetricsService(
+  private val offenderSearchApiService: OffenderSearchApiService,
+  private val prisonRegisterApiService: PrisonRegisterApiService,
+  private val registry: MeterRegistry,
+) {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
+  suspend fun recordPrisonersCountForEachPrison() {
+    val prisonList = prisonRegisterApiService.getActivePrisonsList()
+    var prisonersCount = 0
+    var prisoners12WeeksCount = 0
+    var prisoners24WeeksCount = 0
+    var prisonersAllTimeCount = 0
+    val earliestReleaseDate = LocalDate.now().minusDays(1)
+    val latestRD12Weeks = LocalDate.now().plusDays(84)
+    val latestRD24Weeks = LocalDate.now().plusDays(168)
+    for (item in prisonList) {
+      try {
+        if (item.active) {
+          offenderSearchApiService.findPrisonersBySearchTerm(item.id, "").collect {
+            it.forEach {
+              if (it.confirmedReleaseDate != null) {
+                it.displayReleaseDate = it.confirmedReleaseDate
+              } else if (it.actualParoleDate != null) {
+                it.displayReleaseDate = it.actualParoleDate
+              } else if (it.homeDetentionCurfewActualDate != null) {
+                it.displayReleaseDate = it.homeDetentionCurfewActualDate
+              } else if (it.conditionalReleaseDate != null) {
+                it.displayReleaseDate = it.conditionalReleaseDate
+              } else if (it.automaticReleaseDate != null) {
+                it.displayReleaseDate = it.automaticReleaseDate
+              } else {
+                it.displayReleaseDate = null
+              }
+              prisonersCount++
+              if (it.displayReleaseDate != null && (it.displayReleaseDate!! > earliestReleaseDate || it.displayReleaseDate!! < latestRD12Weeks)) {
+                prisoners12WeeksCount++
+              }
+              if (it.displayReleaseDate != null && (it.displayReleaseDate!! > earliestReleaseDate || it.displayReleaseDate!! < latestRD24Weeks)) {
+                prisoners24WeeksCount++
+              }
+              if (it.displayReleaseDate != null && (it.displayReleaseDate!! < earliestReleaseDate)) {
+                prisonersAllTimeCount++
+              }
+            }
+            registry.gauge("total_prisoners_count", Tags.of("prison", item.name), prisonersCount)
+            registry.gauge("total_prisoners_12Weeks_count", Tags.of("prison", item.name), prisoners12WeeksCount)
+            registry.gauge("total_prisoners_24Weeks_count", Tags.of("prison", item.name), prisoners24WeeksCount)
+            registry.gauge("total_prisoners_AllTime_count", Tags.of("prison", item.name), prisonersAllTimeCount)
+          }
+        }
+      } catch (ex: Exception) {
+        log.warn("Prisoners data not found. Unable to fetch Prisoners for the Prison Id ${item.id}")
+      }
+    }
+  }
+}

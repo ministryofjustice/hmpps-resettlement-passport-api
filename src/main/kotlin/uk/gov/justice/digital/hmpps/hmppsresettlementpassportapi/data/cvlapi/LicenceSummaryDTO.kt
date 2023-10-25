830101760fbdaeee3ba028e1198b732b63f942f2
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-10-25 13:26:33 +0100

    RP2-732 Fix 500 error on get licence conditions (#160)
    * RP2-732 Fix 500 error on get licence conditions
    
    * Fix formatting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/cvlapi/LicenceSummaryDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/cvlapi/LicenceSummaryDTO.kt
@@ -2,29 +2,8 @@ package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.cvlapi
 
 data class LicenceSummary(
   val licenceId: Long,
-  val licenceType: String,
   val licenceStatus: String,
-  val nomsId: String,
-  val surname: String,
-  val forename: String,
-  val prisonCode: String,
-  val prisonDescription: String,
-  val probationAreaCode: String,
-  val probationAreaDescription: String,
-  val probationPduCode: String,
-  val probationPduDescription: String,
-  val probationLauCode: String,
-  val probationLauDescription: String,
-  val probationTeamCode: String,
-  val conditionalReleaseDate: String,
-  val actualReleaseDate: String,
-  val crn: String,
-  val dateOfBirth: String,
-  val comUsername: String,
-  val bookingId: String,
-  val dateCreated: String,
-  val approvedByName: String? = null,
-  val approvedDate: String? = null,
+  val dateCreated: String?,
 )
 
 data class LicenceRequest(

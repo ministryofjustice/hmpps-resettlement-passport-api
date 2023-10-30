f95b1c0775efc62dfb61cd5763cd7687fb5cb684
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-10-30 12:11:56 +0000

    RP2-703 Metrics count for Inuse Prisoners (#173)
    * RP2-703 Metrics count for Inuse Prisoners
    
    * RP2-703 Metrics count for InUse Prisoners review fix
    
    * RP2-703 review fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/PathwayStatusRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/PathwayStatusRepository.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PathwayEntity
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PathwayStatusEntity
@@ -9,4 +10,7 @@ import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.Pris
 @Repository
 interface PathwayStatusRepository : JpaRepository<PathwayStatusEntity, Long> {
   fun findByPathwayAndPrisoner(pathway: PathwayEntity, prisoner: PrisonerEntity): PathwayStatusEntity?
+
+  @Query("select p.nomsId from PathwayStatusEntity ps, PrisonerEntity p where ps.status.id != :status and p.prisonId = :prisonId and p.id = ps.prisoner.id group by p.nomsId having count(ps) > 0")
+  fun findInUsePrisonersByPrisonIdAndStatus(prisonId: String, status: Long): List<String>
 }

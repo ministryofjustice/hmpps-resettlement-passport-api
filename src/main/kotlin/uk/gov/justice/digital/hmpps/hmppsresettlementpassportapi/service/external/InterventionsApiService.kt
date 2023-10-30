b69a6d1834a1e6013e9aa1cbc4e6736d111e8e14
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-10-30 16:13:04 +0000

    Fix linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/external/InterventionsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/external/InterventionsApiService.kt
@@ -2,9 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.extern
 
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpClientErrorException.NotFound
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.interventionsapi.ReferralDTO
@@ -23,8 +21,7 @@ class InterventionsApiService(
       {
         it is WebClientResponseException && it.statusCode.isSameCodeAs(HttpStatus.NOT_FOUND)
       },
-      listOf()
+      listOf(),
     )
     .awaitSingle()
-
 }

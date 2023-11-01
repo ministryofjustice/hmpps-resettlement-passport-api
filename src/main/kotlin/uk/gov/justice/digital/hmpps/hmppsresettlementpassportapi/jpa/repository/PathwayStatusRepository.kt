60e698e45bff19b04b04d75988bbcc69a0e3d76d
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2023-11-01 16:28:36 +0000

    RP2-683 Update metrics for prisoners (#178)
    * RP2-683 Update metrics for prisoners
    
    * Fix formatting
    
    * Fix typo
    
    ---------
    
    Co-authored-by: James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/PathwayStatusRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/PathwayStatusRepository.kt
@@ -3,17 +3,22 @@ package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.Pathway
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PathwayEntity
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PathwayStatusEntity
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PrisonerEntity
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.Status
 
 @Repository
 interface PathwayStatusRepository : JpaRepository<PathwayStatusEntity, Long> {
   fun findByPathwayAndPrisoner(pathway: PathwayEntity, prisoner: PrisonerEntity): PathwayStatusEntity?
 
   @Query("select p.nomsId from PathwayStatusEntity ps, PrisonerEntity p where ps.status.id != :status and p.prisonId = :prisonId and p.id = ps.prisoner.id group by p.nomsId having count(ps) > 0")
-  fun findInUsePrisonersByPrisonIdAndStatus(prisonId: String, status: Long): List<String>
+  fun findPrisonersByPrisonIdWithAtLeastOnePathwayNotInNotStarted(prisonId: String, status: Long = Status.NOT_STARTED.id): List<String>
 
-  @Query("select p.nomsId from PathwayStatusEntity ps, PrisonerEntity p where ps.status.id = :status and p.prisonId = :prisonId and p.id = ps.prisoner.id group by p.nomsId having count(ps) = 7")
-  fun findNotStartedPrisonersByPrisonIdAndStatus(prisonId: String, status: Long): List<String>
+  @Query("select p.nomsId from PathwayStatusEntity ps, PrisonerEntity p where ps.status.id = :status and p.prisonId = :prisonId and p.id = ps.prisoner.id group by p.nomsId having count(ps) = :numberOfPathways")
+  fun findPrisonersByPrisonIdWithAllPathwaysNotStarted(prisonId: String, status: Long = Status.NOT_STARTED.id, numberOfPathways: Int = Pathway.values().size): List<String>
+
+  @Query("select p.nomsId from PathwayStatusEntity ps, PrisonerEntity p where ps.status.id = :status and p.prisonId = :prisonId and p.id = ps.prisoner.id group by p.nomsId having count(ps) = :numberOfPathways")
+  fun findPrisonersByPrisonWithAllPathwaysDone(prisonId: String, status: Long = Status.DONE.id, numberOfPathways: Int = Pathway.values().size): List<String>
 }

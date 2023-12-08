9c0d751363db5a4eba8ab88a91850363b478bcfb
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-12-08 16:19:31 +0000

    RP2-901 Fix further issue with equalities in metrics calcs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/MetricsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/MetricsService.kt
@@ -53,8 +53,8 @@ class MetricsService(
 
         val allPrisonersPastCount = prisoners.filter { it.displayReleaseDate != null && it.displayReleaseDate!! < earliestFutureReleaseDate }.size
         val allPrisonersFutureCount = prisoners.filter { it.displayReleaseDate == null || it.displayReleaseDate!! >= earliestFutureReleaseDate }.size
-        val allPrisoners12WeeksCount = prisoners.filter { it.displayReleaseDate != null && it.displayReleaseDate!! >= earliestFutureReleaseDate && it.displayReleaseDate!! < latestRD12Weeks }.size
-        val allPrisoners24WeeksCount = prisoners.filter { it.displayReleaseDate != null && it.displayReleaseDate!! >= earliestFutureReleaseDate && it.displayReleaseDate!! < latestRD24Weeks }.size
+        val allPrisoners12WeeksCount = prisoners.filter { it.displayReleaseDate != null && it.displayReleaseDate!! >= earliestFutureReleaseDate && it.displayReleaseDate!! <= latestRD12Weeks }.size
+        val allPrisoners24WeeksCount = prisoners.filter { it.displayReleaseDate != null && it.displayReleaseDate!! >= earliestFutureReleaseDate && it.displayReleaseDate!! <= latestRD24Weeks }.size
 
         val notStartedPrisonersPastCount = prisonerService.getNotStartedPrisonersByPrisonId(prison.id, earliestPastReleaseDate, latestPastReleaseDate).filter { it.releaseDate != null }.size
         val notStartedPrisonersFutureCount = prisonerService.getNotStartedPrisonersByPrisonId(prison.id, earliestFutureReleaseDate, latestFutureReleaseDate).size

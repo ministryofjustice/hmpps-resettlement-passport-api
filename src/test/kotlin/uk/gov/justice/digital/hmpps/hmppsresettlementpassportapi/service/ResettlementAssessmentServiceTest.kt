065f1f57a8bbe5ac84ac1f4e6b8b6754aab03172
Emily Pritchard <emily.pritchard@hippodigital.co.uk>
2024-01-11 16:50:50 +0000

    Unit test possibly needs refactoring

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/ResettlementAssessmentServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/ResettlementAssessmentServiceTest.kt
@@ -45,23 +45,42 @@ class ResettlementAssessmentServiceTest {
     resettlementAssessmentService = ResettlementAssessmentService(resettlementAssessmentRepository, prisonerRepository, pathwayRepository)
   }
 
-//  @Test
-//  fun `test getResettlementAssessmentSummaryByNomsId - returns assessment`() {
-//    val nomsId: String = "GY3245"
-//    val prisonerEntity = PrisonerEntity(1, "GY3245", testDate, "crn", "xyz1", LocalDate.parse("2025-01-23"))
-//    val pathwayEntity = PathwayEntity()
-//    val prisonerResettlementAssessmentSummary = [
-//      PrisonerResettlementAssessment(Pathway.ACCOMMODATION, ResettlementAssessmentStatus.NOT_STARTED),
-//      PrisonerResettlementAssessment(Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, ResettlementAssessmentStatus.NOT_STARTED),
-//      PrisonerResettlementAssessment(Pathway.CHILDREN_FAMILIES_AND_COMMUNITY, ResettlementAssessmentStatus.NOT_STARTED),
-//      PrisonerResettlementAssessment(Pathway.DRUGS_AND_ALCOHOL, ResettlementAssessmentStatus.NOT_STARTED),
-//      PrisonerResettlementAssessment(Pathway.EDUCATION_SKILLS_AND_WORK, ResettlementAssessmentStatus.NOT_STARTED),
-//      PrisonerResettlementAssessment(Pathway.FINANCE_AND_ID, ResettlementAssessmentStatus.NOT_STARTED),
-//      PrisonerResettlementAssessment(Pathway.HEALTH, ResettlementAssessmentStatus.NOT_STARTED),
-//    Mockito.`when`(resettlementAssessmentRepository.findFirstByPrisonerAndPathwayAndAssessmentTypeOrderByCreationDateDesc(prisonerEntity, )  ..findById(assessmentId)).thenReturn(Optional.of(assessmentEntity))
-//
-//    val response = assessmentService.getAssessmentById(assessmentId)
-//    Assertions.assertEquals(assessmentEntity, response)
-//  }
+  @Test
+  fun `test getResettlementAssessmentSummaryByNomsId - returns assessment`() {
+    val nomsId: String = "GY3245"
+    val assessmentType = ResettlementAssessmentType.BCST2
+    val prisonerEntity = PrisonerEntity(1, "GY3245", testDate, "crn", "xyz1", LocalDate.parse("2025-01-23"))
+    val accommodationPathwayEntity = PathwayEntity(1, "Accommodation", true, fakeNow)
+    val accommodationResettlementAssessmentEntity = createResettlementAssessmentEntity(1, "Accommodation")
+    val
+
+    val prisonerResettlementAssessmentSummary = [
+      PrisonerResettlementAssessment(Pathway.ACCOMMODATION, ResettlementAssessmentStatus.NOT_STARTED),
+      PrisonerResettlementAssessment(Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, ResettlementAssessmentStatus.NOT_STARTED),
+      PrisonerResettlementAssessment(Pathway.CHILDREN_FAMILIES_AND_COMMUNITY, ResettlementAssessmentStatus.NOT_STARTED),
+      PrisonerResettlementAssessment(Pathway.DRUGS_AND_ALCOHOL, ResettlementAssessmentStatus.NOT_STARTED),
+      PrisonerResettlementAssessment(Pathway.EDUCATION_SKILLS_AND_WORK, ResettlementAssessmentStatus.NOT_STARTED),
+      PrisonerResettlementAssessment(Pathway.FINANCE_AND_ID, ResettlementAssessmentStatus.NOT_STARTED),
+      PrisonerResettlementAssessment(Pathway.HEALTH, ResettlementAssessmentStatus.NOT_STARTED),
+
+    Mockito.`when`(resettlementAssessmentRepository.findFirstByPrisonerAndPathwayAndAssessmentTypeOrderByCreationDateDesc(prisonerEntity, accommodationPathwayEntity, assessmentType)).thenReturn(accommodationResettlementAssessmentEntity))
+
+    val response = resettlementAssessmentService.getResettlementAssessmentSummaryByNomsId(nomsId)
+    Assertions.assertEquals(prisonerResettlementAssessmentSummary, response)
+
+
+
+  }
+
+  private fun createResettlementAssessmentEntity(id: Long, name: String) = ResettlementAssessmentEntity(
+    id= id,
+    prisoner = PrisonerEntity(1, "GY3245", testDate, "crn", "xyz1", LocalDate.parse("2025-01-23")),
+    pathway = PathwayEntity(id = id, name = name, active=true, fakeNow),
+    assessmentType = ResettlementAssessmentType.BCST2,
+    assessmentStatus = ResettlementAssessmentStatusEntity(id = 1, name = "Not Started", true, fakeNow),
+    assessment = "assessment",
+    creationDate = fakeNow,
+    createdBy = "PO",
+    statusChangedTo = null )
 
 }

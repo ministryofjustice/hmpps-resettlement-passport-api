dda16fec8ad1c5b4840fbf779ada248ef9a2fc2a
Emily Pritchard <107629810+EmilyPri@users.noreply.github.com>
2024-01-12 16:09:08 +0000

    RP2 970- Get Assessment Summary endpoint (#215)
    * Service built, unit tests to write
    
    * Integration tests passing
    
    * Unit test possibly needs refactoring
    
    * Unit tests refactored, all tests passing
    
    * Linting
    
    * Unit tests passing
    
    * Linting 2
    
    * Update src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/ResettlementAssessmentController.kt
    
    Co-authored-by: James Boobier <139986710+JamesBoobier@users.noreply.github.com>
    
    * Code review
    
    * Code review 2
    
    * Reverted ResettlementAssessmentRepository
    
    ---------
    
    Co-authored-by: James Boobier <139986710+JamesBoobier@users.noreply.github.com>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/ResettlementAssessmentService.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service
+
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config.ResourceNotFoundException
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.PrisonerResettlementAssessment
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.Pathway
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.ResettlementAssessmentStatus
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.ResettlementAssessmentType
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.PathwayRepository
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.PrisonerRepository
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.ResettlementAssessmentRepository
+
+@Service
+class ResettlementAssessmentService(
+  private val resettlementAssessmentRepository: ResettlementAssessmentRepository,
+  private val prisonerRepository: PrisonerRepository,
+  private val pathwayRepository: PathwayRepository,
+) {
+  @Transactional
+  fun getResettlementAssessmentSummaryByNomsId(nomsId: String): List<PrisonerResettlementAssessment> {
+    val prisonerEntity = prisonerRepository.findByNomsId(nomsId)
+      ?: throw ResourceNotFoundException("Prisoner with id $nomsId not found in database")
+    val pathways = Pathway.entries.toTypedArray()
+    return pathways.map {
+      val pathwayEntity = pathwayRepository.findById(it.id).get()
+      val resettlementAssessmentForPathway =
+        resettlementAssessmentRepository.findFirstByPrisonerAndPathwayAndAssessmentTypeOrderByCreationDateDesc(
+          prisonerEntity,
+          pathwayEntity,
+          ResettlementAssessmentType.BCST2,
+        )
+      if (resettlementAssessmentForPathway == null) {
+        PrisonerResettlementAssessment(it, ResettlementAssessmentStatus.NOT_STARTED)
+      } else {
+        PrisonerResettlementAssessment(
+          it,
+          ResettlementAssessmentStatus.getById(resettlementAssessmentForPathway.assessmentStatus.id),
+        )
+      }
+    }
+  }
+}

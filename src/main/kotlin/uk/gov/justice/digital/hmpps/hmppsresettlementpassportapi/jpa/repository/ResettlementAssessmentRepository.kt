2c1a1b5f10ad0f6dd3118736552eef9cfe62ff84
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2024-02-02 14:01:40 +0000

    RP-912 Add assessmentRequired flag to prisoners

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/ResettlementAssessmentRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/ResettlementAssessmentRepository.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PathwayEntity
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PrisonerEntity
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.ResettlementAssessmentEntity
@@ -18,4 +19,10 @@ interface ResettlementAssessmentRepository : JpaRepository<ResettlementAssessmen
   ): ResettlementAssessmentEntity?
 
   fun findByPrisoner(prisoner: PrisonerEntity): ResettlementAssessmentEntity?
+
+  @Query("select count(distinct(pathway)) from ResettlementAssessmentEntity where prisoner.nomsId = :nomsId and assessmentType = :assessmentType and assessmentStatus.id = :assessmentStatus")
+  fun countByNomsIdAndAssessmentTypeAndAssessmentStatus(nomsId: String, assessmentType: ResettlementAssessmentType, assessmentStatus: Long): Int
+
+  @Query("select ra.prisoner from ResettlementAssessmentEntity ra inner join PrisonerEntity p on ra.prisoner = p where p.prisonId = :prisonId and ra.assessmentType = :assessmentType and ra.assessmentStatus.id = :assessmentStatus group by ra.prisoner having count(distinct (ra.pathway)) = :numOfPathways")
+  fun findPrisonersByPrisonIdAndAssessmentTypeAndAssessmentStatus(prisonId: String, assessmentType: ResettlementAssessmentType, assessmentStatus: Long, numOfPathways: Int): List<PrisonerEntity>
 }

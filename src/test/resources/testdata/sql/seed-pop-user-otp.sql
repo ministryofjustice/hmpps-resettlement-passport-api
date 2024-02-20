a75681f716318b080bcb95fb95216afb34958112
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2024-02-20 11:57:51 +0000

    PLT-43 Generate One Time Password (OTP) - for digital (lives in PSFR)  (#239)
    * PLT-43 Generate One Time Password (OTP) - for digital (lives in PSFR)
    
    * PLT-43 Generate One Time Password (OTP) - for digital (lives in PSFR)
    
    * PLT-43 Removed the commented code
    
    * PLT-43 fix for linting issue

---
+++ src/test/resources/testdata/sql/seed-pop-user-otp.sql
@@ -0,0 +1,7 @@
+
+DELETE from person_on_probation_user_otp;
+DELETE from prisoner;
+
+INSERT INTO prisoner
+(id, noms_id, creation_date, crn)
+VALUES(1, 'G4161UF', '2024-02-19T09:36:28.713421','abc');

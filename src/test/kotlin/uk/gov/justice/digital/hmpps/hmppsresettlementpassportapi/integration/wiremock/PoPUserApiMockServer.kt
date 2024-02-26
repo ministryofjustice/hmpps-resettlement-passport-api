6ad62bcda3ead53c4b1742a5b9fafc1b18378c59
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-02-26 09:00:48 +0000

    PLT-41 Validate the Prisoner against the PSFR Tool using the verify endpoints in PoP User Tool

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/wiremock/PoPUserApiMockServer.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.wiremock
+
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.readFile
+
+class PoPUserApiMockServer : WireMockServerBase(9106) {
+
+  fun stubPostPoPUserVerification(status: Int) {
+    val popUserDataResponseJSON = readFile("testdata/pop-user-api/pop-user-verify-response.json")
+    val popUserRequestJSON = readFile("testdata/pop-user-api/pop-user-request.json")
+    stubFor(
+      WireMock.post("/person-on-probation-user/user")
+        .withRequestBody(WireMock.equalToJson(popUserRequestJSON, true, true))
+        .willReturn(
+          if (status == 200) {
+            aResponse()
+              .withHeader("Content-Type", "application/json")
+              .withBody(
+                popUserDataResponseJSON,
+              )
+              .withStatus(status)
+          } else {
+            aResponse()
+              .withHeader("Content-Type", "application/json")
+              .withBody("{\"Error\" : \"$status\"}")
+              .withStatus(status)
+          },
+        ),
+    )
+  }
+}

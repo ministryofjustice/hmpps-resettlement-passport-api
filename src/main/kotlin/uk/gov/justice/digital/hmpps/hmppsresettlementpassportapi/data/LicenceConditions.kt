565388680bac9d09883371bb40ea765b5fd93d5b
riccardo-noviello-moj <159039282+riccardo-noviello-moj@users.noreply.github.com>
2024-03-06 16:32:07 +0000

    PLT-57 add missing licence condition mapping (#265)
    * PLT-57 add missing license condition mapping
    
    * PLT-57 add missing license condition mapping

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/LicenceConditions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/LicenceConditions.kt
@@ -2,6 +2,8 @@ package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data
 data class LicenceConditions(
   var licenceId: Long?,
   var status: String?,
+  var startDate: String?,
+  var expiryDate: String?,
 
   var standardLicenceConditions: List<Conditions>? = emptyList(),
   var otherLicenseConditions: List<Conditions>? = emptyList(),
@@ -11,4 +13,5 @@ data class Conditions(
   var id: Long? = null,
   var image: Boolean,
   var text: String? = null,
+  var sequence: Int? = null,
 )

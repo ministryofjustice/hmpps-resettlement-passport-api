657677033c881b6a3e9a1d931575b5e843ca6fdf
mkerry-moj <157010890+mkerry-moj@users.noreply.github.com>
2024-03-12 17:11:58 +0000

    PSFR-933 Adjust attitudes thinking and behaviour (#273)
    assessment

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/AttitudesThinkingAndBehaviourResettlementAssessmentStrategyTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/AttitudesThinkingAndBehaviourResettlementAssessmentStrategyTest.kt
@@ -100,62 +100,28 @@ class AttitudesThinkingAndBehaviourResettlementAssessmentStrategyTest {
       null,
       "HELP_TO_MANAGE_ANGER",
     ),
-    // Any answer to HELP_TO_MANAGE_ANGER, go to INFLUENCED_BY_OTHERS
+    // Any answer to HELP_TO_MANAGE_ANGER, go to ISSUES_WITH_GAMBLING
     Arguments.of(
-      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(
+      listOf(
         ResettlementAssessmentRequestQuestionAndAnswer("HELP_TO_MANAGE_ANGER", answer = StringAnswer("YES")),
       ),
       "HELP_TO_MANAGE_ANGER",
-      "INFLUENCED_BY_OTHERS",
-    ),
-    // Any answer to INFLUENCED_BY_OTHERS, go to ISSUES_WITH_GAMBLING
-    Arguments.of(
-      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(
-        ResettlementAssessmentRequestQuestionAndAnswer("HELP_TO_MANAGE_ANGER", answer = StringAnswer("YES")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INFLUENCED_BY_OTHERS", answer = StringAnswer("NO")),
-      ),
-      "INFLUENCED_BY_OTHERS",
       "ISSUES_WITH_GAMBLING",
     ),
-    // Any answer to ISSUES_WITH_GAMBLING, go to INVOLVED_IN_GANG_ACTIVITY
+    // Any answer to ISSUES_WITH_GAMBLING, go to ASSESSMENT_SUMMARY
     Arguments.of(
-      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(
+      listOf(
         ResettlementAssessmentRequestQuestionAndAnswer("HELP_TO_MANAGE_ANGER", answer = StringAnswer("YES")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INFLUENCED_BY_OTHERS", answer = StringAnswer("NO")),
         ResettlementAssessmentRequestQuestionAndAnswer("ISSUES_WITH_GAMBLING", answer = StringAnswer("NO_ANSWER")),
       ),
       "ISSUES_WITH_GAMBLING",
-      "INVOLVED_IN_GANG_ACTIVITY",
-    ),
-    // Any answer to INVOLVED_IN_GANG_ACTIVITY, go to UNDER_THREAT_OUTSIDE_PRISON
-    Arguments.of(
-      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(
-        ResettlementAssessmentRequestQuestionAndAnswer("HELP_TO_MANAGE_ANGER", answer = StringAnswer("YES")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INFLUENCED_BY_OTHERS", answer = StringAnswer("NO")),
-        ResettlementAssessmentRequestQuestionAndAnswer("ISSUES_WITH_GAMBLING", answer = StringAnswer("NO_ANSWER")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INVOLVED_IN_GANG_ACTIVITY", answer = StringAnswer("NO")),
-      ),
-      "INVOLVED_IN_GANG_ACTIVITY",
-      "UNDER_THREAT_OUTSIDE_PRISON",
-    ),
-    // Any answer to UNDER_THREAT_OUTSIDE_PRISON, go to ASSESSMENT_SUMMARY
-    Arguments.of(
-      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(
-        ResettlementAssessmentRequestQuestionAndAnswer("HELP_TO_MANAGE_ANGER", answer = StringAnswer("YES")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INFLUENCED_BY_OTHERS", answer = StringAnswer("NO")),
-        ResettlementAssessmentRequestQuestionAndAnswer("ISSUES_WITH_GAMBLING", answer = StringAnswer("NO_ANSWER")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INVOLVED_IN_GANG_ACTIVITY", answer = StringAnswer("NO")),
-      ),
-      "UNDER_THREAT_OUTSIDE_PRISON",
       "ASSESSMENT_SUMMARY",
     ),
     // Any answer to ASSESSMENT_SUMMARY, go to CHECK_ANSWERS
     Arguments.of(
-      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(
+      listOf(
         ResettlementAssessmentRequestQuestionAndAnswer("HELP_TO_MANAGE_ANGER", answer = StringAnswer("YES")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INFLUENCED_BY_OTHERS", answer = StringAnswer("NO")),
         ResettlementAssessmentRequestQuestionAndAnswer("ISSUES_WITH_GAMBLING", answer = StringAnswer("NO_ANSWER")),
-        ResettlementAssessmentRequestQuestionAndAnswer("INVOLVED_IN_GANG_ACTIVITY", answer = StringAnswer("NO")),
       ),
       "ASSESSMENT_SUMMARY",
       "CHECK_ANSWERS",
@@ -187,30 +153,12 @@ class AttitudesThinkingAndBehaviourResettlementAssessmentStrategyTest {
           ResettlementAssessmentResponseQuestionAndAnswer(
             question = ResettlementAssessmentResponseQuestion(
               id = "HELP_TO_MANAGE_ANGER",
-              title = "Does the person in prison need support managing their anger?",
+              title = "Does the person in prison want support managing their emotions?",
               subTitle = null,
               type = TypeOfQuestion.RADIO,
               options = yesNoOptions.toMutableList(),
             ),
             originalPageId = "HELP_TO_MANAGE_ANGER",
-          ),
-        ),
-      ),
-    ),
-    Arguments.of(
-      "INFLUENCED_BY_OTHERS",
-      ResettlementAssessmentResponsePage(
-        id = "INFLUENCED_BY_OTHERS",
-        questionsAndAnswers = mutableListOf(
-          ResettlementAssessmentResponseQuestionAndAnswer(
-            question = ResettlementAssessmentResponseQuestion(
-              id = "INFLUENCED_BY_OTHERS",
-              title = "Is the person in prison influenced by others to do things they don’t want to do?",
-              subTitle = null,
-              type = TypeOfQuestion.RADIO,
-              options = yesNoOptions.toMutableList(),
-            ),
-            originalPageId = "INFLUENCED_BY_OTHERS",
           ),
         ),
       ),
@@ -223,7 +171,7 @@ class AttitudesThinkingAndBehaviourResettlementAssessmentStrategyTest {
           ResettlementAssessmentResponseQuestionAndAnswer(
             question = ResettlementAssessmentResponseQuestion(
               id = "ISSUES_WITH_GAMBLING",
-              title = "Does the person in prison have any issues with gambling?",
+              title = "Does the person in prison want support with gambling issues?",
               subTitle = null,
               type = TypeOfQuestion.RADIO,
               options = yesNoOptions.toMutableList(),

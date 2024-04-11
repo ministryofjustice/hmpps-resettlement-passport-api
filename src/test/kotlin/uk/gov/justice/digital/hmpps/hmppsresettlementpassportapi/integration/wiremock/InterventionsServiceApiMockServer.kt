121a3bd04dfe45f6a12b1faa6f8e6af749b39334
Peter Carter <139152930+peterhippocarter@users.noreply.github.com>
2024-04-11 15:11:56 +0100

    Rp2 1176 (#298)
    * ensure all files have new line char(s)
    
    * restore missing curly brace
    
    * revert migration sql
    
    * restore rn on sql script that already had them
    
    * reversions to original /2...
    
    * additional reversions
    
    * use git checkout to revert

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/wiremock/InterventionsServiceApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/wiremock/InterventionsServiceApiMockServer.kt
@@ -1,5 +1,3 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.wiremock
 
-import com.github.tomakehurst.wiremock.client.WireMock.get
-
 class InterventionsServiceApiMockServer : WireMockServerBase(9105)

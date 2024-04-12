23afb0d53e791853bf28e00da8552cfde3c764be
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2024-04-12 16:55:11 +0100

    Add tests

---
+++ src/test/resources/testdata/sql/test-database-v1-37.sql
@@ -0,0 +1,85 @@
+INSERT INTO prisoner (id, noms_id, creation_date, crn, prison_id) VALUES (1, 'G1458GV', '2023-05-17 12:21:44.0', 'CRN1', 'xyz');
+INSERT INTO prisoner (id, noms_id, creation_date, crn, prison_id) VALUES (2, 'G6628UE', '2023-05-17 12:21:44.0', 'CRN2', 'xyz');
+INSERT INTO prisoner (id, noms_id, creation_date, crn, prison_id) VALUES (3, 'G6335VX', '2023-05-17 12:21:44.0', 'CRN3', 'xyz');
+INSERT INTO prisoner (id, noms_id, creation_date, crn, prison_id) VALUES (4, 'G6933GF', '2023-05-17 12:21:44.0', 'CRN4', 'xyz');
+INSERT INTO prisoner (id, noms_id, creation_date, crn, prison_id) VALUES (5, 'A8339DY', '2023-05-17 12:21:44.0', 'CRN5', 'xyz');
+
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (1, 1, 1, 1, '2023-05-17 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (2, 1, 2, 2, '2023-05-18 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (3, 1, 3, 3, '2023-05-19 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (4, 1, 4, 4, '2023-05-20 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (5, 1, 5, 5, '2023-05-21 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (6, 1, 6, 1, '2023-05-22 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (7, 1, 7, 2, '2023-05-23 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (8, 2, 1, 3, '2023-05-24 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (9, 2, 2, 4, '2023-05-25 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (10, 2, 3, 5, '2023-05-26 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (11, 2, 4, 1, '2023-05-27 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (12, 2, 5, 2, '2023-05-28 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (13, 2, 6, 3, '2023-05-29 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (14, 2, 7, 4, '2023-05-30 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (15, 3, 1, 5, '2023-05-31 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (16, 3, 2, 1, '2023-06-01 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (17, 3, 3, 2, '2023-06-02 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (18, 3, 4, 3, '2023-06-03 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (19, 3, 5, 4, '2023-06-04 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (20, 3, 6, 5, '2023-06-05 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (21, 3, 7, 1, '2023-06-06 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (22, 4, 1, 2, '2023-06-07 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (23, 4, 2, 3, '2023-06-08 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (24, 4, 3, 4, '2023-06-09 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (25, 4, 4, 5, '2023-06-10 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (26, 4, 5, 1, '2023-06-11 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (27, 4, 6, 2, '2023-06-12 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (28, 4, 7, 3, '2023-06-13 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (29, 5, 1, 4, '2023-06-14 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (30, 5, 2, 5, '2023-06-15 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (31, 5, 3, 1, '2023-06-16 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (32, 5, 4, 2, '2023-06-17 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (33, 5, 5, 3, '2023-06-18 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (34, 5, 6, 4, '2023-06-19 12:21:44.0');
+INSERT INTO pathway_status (id, prisoner_id, pathway_id, status_id, updated_date) VALUES (35, 5, 7, 5, '2023-06-20 12:21:44.0');
+
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(1, 1, 1, 4, 'BCST2', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(2, 1, 2, 4, 'BCST2', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(3, 1, 3, 4, 'BCST2', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(4, 1, 4, 4, 'BCST2', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(5, 1, 5, 4, 'BCST2', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(6, 1, 6, 4, 'BCST2', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(7, 1, 7, 4, 'BCST2', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(8, 1, 1, 4, 'RESETTLEMENT_PLAN', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(9, 1, 2, 4, 'RESETTLEMENT_PLAN', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(11, 1, 3, 4, 'RESETTLEMENT_PLAN', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(12, 1, 4, 4, 'RESETTLEMENT_PLAN', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(13, 1, 5, 4, 'RESETTLEMENT_PLAN', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(14, 1, 6, 4, 'RESETTLEMENT_PLAN', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');
+INSERT INTO resettlement_assessment
+(id, prisoner_id, pathway_id, assessment_status_id, assessment_type, assessment, status_changed_to_status_id, created_date, created_by)
+VALUES(15, 1, 7, 4, 'RESETTLEMENT_PLAN', '{"assessment": []}'::jsonb, 1, '2023-10-16 12:21:38.709','Prison Officer');

45e74f45bceebe3c63d0e45d4070d9dd95ec7226
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-04-29 17:45:35 +0100

    PLT-224 percentage missing licence condition

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/POPUserMetricsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/POPUserMetricsService.kt
@@ -1,63 +1,85 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service
 
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonapi.Prison
-import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PrisonerEntity
-import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.external.PoPUserApiService
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config.ResourceNotFoundException
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.LicenceTag
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.PopUserCountMetric
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.PopUserCountMetrics
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.external.PrisonRegisterApiService
-import java.util.stream.Stream
 
 @Service
 class POPUserMetricsService(
   private val registry: MeterRegistry,
-  private val prisonOTPService: PoPUserOTPService,
-  private val poPUserApiService: PoPUserApiService,
+  private val popUserOTPService: PoPUserOTPService,
   private val prisonRegisterApiService: PrisonRegisterApiService,
-  private val prisonerService: PrisonerService,
-  @Value("\${cron.release-dates.batch-size:200}") val batchSize: Int,
+  private val licenceConditionService: LicenceConditionService,
 ) {
+  private val popUserCountMetrics = PopUserCountMetrics()
 
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
   }
 
   fun recordCustomMetrics() {
-    recordProbationUsersAppointmentMetrics()
+    recordProbationUsersLicencesConditionMetrics()
   }
 
-  fun recordProbationUsersAppointmentMetrics(): Int {
-    val otpList = prisonOTPService.getAllOTPs()
+  fun recordProbationUsersLicencesConditionMetrics() {
+    val popUserList = popUserOTPService.getAllOTPs()
 
+    var totalPopUser = 0
+    if (popUserList != null) {
+      totalPopUser = popUserList.size
+    }
     val prisonList = prisonRegisterApiService.getActivePrisonsList()
-    var slice = prisonerService.getSliceOfAllPrisoners(PageRequest.of(0, batchSize).withSort(Sort.by(Sort.Direction.ASC, "id")))
-    var cnt = countPopUsers(slice.get())
-    while (slice.hasNext()) {
-      slice = prisonerService.getSliceOfAllPrisoners((slice.nextPageable()))
-      cnt+=countPopUsers(slice.get())
-    }
-    for (prison in prisonList) {
-      val cntByPrison = otpList?.size ?: 0
-      registry.gauge(
-        "total_pack_print_count",
-        cnt,
-      )
-      return cnt
-    }
-    return 0
-  }
+    if (popUserList != null) {
+      for (prison in prisonList) {
+        var stdLicenceConditionCount = 0
+        var otherLicenceConditionCount = 0
+        var percentStdLicenceCondition = 0
+        var percentOtherLicenceCondition = 0
+        try {
+          for (popUser in popUserList) {
+            if (popUser.prisoner.prisonId.equals(prison.id)) {
+              val licencesConditions = licenceConditionService.getLicenceConditionsByNomsId(popUser.prisoner.nomsId)
+              val stdLicenceConditionList = licencesConditions?.standardLicenceConditions
+              val otherLicenceConditionList = licencesConditions?.otherLicenseConditions
+              if (!stdLicenceConditionList.isNullOrEmpty()) {
+                stdLicenceConditionCount += 1
+              }
+              if (!otherLicenceConditionList.isNullOrEmpty()) {
+                otherLicenceConditionCount += 1
+              }
+            }
+          }
+          percentStdLicenceCondition = (stdLicenceConditionCount / totalPopUser) * 100
+          percentOtherLicenceCondition = (otherLicenceConditionCount / totalPopUser) * 100
+          val metrics = listOf(
+            PopUserCountMetric(LicenceTag.STANDARD, percentStdLicenceCondition),
+            PopUserCountMetric(LicenceTag.OTHERS, percentOtherLicenceCondition),
+          )
+          popUserCountMetrics.metrics[prison] = metrics
+          val prisonTag = Tags.of("prison", prison.name)
 
-  fun countPopUsers(prisonerEntities: Stream<PrisonerEntity>): Int {
-    val activeUserList = poPUserApiService.getVerifiedPopUsers()
-    var cnt = 0;
-    for(popUser in activeUserList) {
-      if (prisonerEntities.anyMatch {value -> popUser.nomsId == value.nomsId})
-        cnt +=1
+          popUserCountMetrics.metrics[prison]?.forEachIndexed { i, metric ->
+            registry.gauge(
+              "total_licence_conditions_percentage",
+              prisonTag
+                .and("licenceType", metric.licenceType.label),
+              popUserCountMetrics,
+            ) {
+              it.metrics[prison]?.get(i)?.value?.toDouble()
+                ?: throw RuntimeException("Can't find value for metric $metric. This is likely a coding error!")
+            }
+          }
+        } catch (_: ResourceNotFoundException) {
+        } catch (ex: Exception) {
+          log.warn("Error collecting metrics for popUser LicenceConditions ${prison.name}", ex)
+        }
+      }
     }
-      return cnt
   }
 }

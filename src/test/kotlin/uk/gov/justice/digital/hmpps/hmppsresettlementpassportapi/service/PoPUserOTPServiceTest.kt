780ae65b11b8113562bb7a8ba62054cb4e7be6df
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-04-29 17:51:34 +0100

    PLT-239 Remove GET popuser/OTP

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/PoPUserOTPServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/PoPUserOTPServiceTest.kt
@@ -253,7 +253,6 @@ class PoPUserOTPServiceTest {
     every { LocalDateTime.now() } returns fakeNow
     val oneLoginUserData = OneLoginData("urn1", "123457", "email@test.com", LocalDate.parse("1982-10-24"))
     val prisoner = PrisonerEntity(1, "acb", fakeNow, "crn", "xyz", null)
-    val popUserResponse = PoPUserResponse(1, "crn1", "NA", true, fakeNow, fakeNow, "GU1234", "urn1")
     val prisonerResponse = PrisonersSearch(
       prisonerNumber = "A123456",
       firstName = "firstName",

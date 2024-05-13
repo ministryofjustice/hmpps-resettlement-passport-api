b9c7270566f33f5fca1d2e52799c07d99f916e04
Ashlie <65076082+ashliecodes@users.noreply.github.com>
2024-05-13 10:46:55 +0100

    PSFR-1303/1304: created watchlist database table and POST endpoint (#327)
    * PSFR-1303/1304: created watchlist database table and POST endpoint
    
    
    * PSFR-1303/1304: made requested changes and added logic for WatchlistService.kt file
    
    
    * PSFR-1303/1304: made requested changes from draft pr and ran formatter

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/entity/WatchlistEntity.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "watchlist")
+class WatchlistEntity(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long?,
+
+  @ManyToOne(cascade = [CascadeType.MERGE])
+  @JoinColumn(name = "prisoner_id", referencedColumnName = "id")
+  val prisoner: PrisonerEntity,
+
+  @Column(name = "staff_username")
+  val staffUsername: String,
+
+  @Column(name = "creation_date")
+  val creationDate: LocalDateTime,
+)

75fc37d05df2464dcdeadebf44645be349969241
Ashlie Prempeh <ashlieprempeh@hotmail.co.uk>
2024-05-20 10:16:27 +0100

    PSFR-1305: changed repository method to write custom query as nomsId is not part of 'WatchlistEntity'

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/WatchlistRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/WatchlistRepository.kt
@@ -1,10 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.WatchlistEntity
 
 @Repository
 interface WatchlistRepository : JpaRepository<WatchlistEntity, Long> {
+  @Query("DELETE FROM WatchlistEntity WHERE prisoner.nomsId = :nomsId and staffUsername = :staffUsername")
   fun deleteByNomsIdAndStaffUsername(nomsId: String, staffUsername: String)
 }

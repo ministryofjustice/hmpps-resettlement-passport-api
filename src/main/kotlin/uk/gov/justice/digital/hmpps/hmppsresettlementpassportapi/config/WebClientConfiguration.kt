30ada278c28fa9f779635d528b7f9881813a7902
Ashlie <65076082+ashliecodes@users.noreply.github.com>
2024-05-24 10:18:22 +0100

    PSFR -1340: Removed CIAG inductions content from PSFR (#361)
    * PSFR-1340: Remove CIAG inductions content from PSFR
    
    * PSFR-1340: Remove CIAG inductions content from PSFR
    
    * PSFR-1340: Renamed WorkReadinessStatus.kt and removed details from json data as integration tests were failing
    
    * PSFR-1340: ran formatter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/config/WebClientConfiguration.kt
@@ -22,7 +22,6 @@ import java.time.Duration
 @Configuration
 @EnableScheduling
 class WebClientConfiguration(
-  @Value("\${api.base.url.oauth}") val authBaseUri: String,
   @Value("\${api.base.url.prison-register}") private val prisonRegisterRootUri: String,
   @Value("\${api.base.url.prisoner-search}") private val prisonerSearchRootUri: String,
   @Value("\${api.base.url.cvl}") private val cvlRootUri: String,
@@ -33,7 +32,6 @@ class WebClientConfiguration(
   @Value("\${api.base.url.allocation-manager}") private val allocationManagerRootUri: String,
   @Value("\${api.base.url.resettlement-passport-delius}") private val rpDeliusRootUri: String,
   @Value("\${api.base.url.education-employment}") private val educationEmploymentRootUri: String,
-  @Value("\${api.base.url.ciag}") private val ciagRootUri: String,
   @Value("\${api.base.url.interventions-service}") private val interventionsRootUri: String,
   @Value("\${api.base.url.pop-user-service}") private val popUserRootUri: String,
   private val objectMapper: ObjectMapper,
@@ -105,11 +103,6 @@ class WebClientConfiguration(
   @Bean
   fun educationEmploymentWebClientCredentials(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
     return getWebClientCredentials(authorizedClientManager, educationEmploymentRootUri)
-  }
-
-  @Bean
-  fun ciagWebClientCredentials(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    return getWebClientCredentials(authorizedClientManager, ciagRootUri)
   }
 
   @Bean

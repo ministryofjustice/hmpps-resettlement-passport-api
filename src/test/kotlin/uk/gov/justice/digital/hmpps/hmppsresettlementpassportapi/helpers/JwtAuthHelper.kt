3f1adc5d927364347e840bc0cc8abc5f32472679
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-05-29 14:01:06 +0100

    PSFR-1367 Add retry to create case note calls

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/helpers/JwtAuthHelper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/helpers/JwtAuthHelper.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.helpers
 
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpHeaders
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
@@ -15,7 +14,7 @@ import java.util.UUID
 
 @Component
 class JwtAuthHelper {
-  private final val keyPair: KeyPair
+  private val keyPair: KeyPair
 
   init {
     val gen = KeyPairGenerator.getInstance("RSA")
@@ -59,11 +58,11 @@ class JwtAuthHelper {
       .also { subject?.let { subject -> it["name"] = subject } }
       .let {
         Jwts.builder()
-          .setId(jwtId)
-          .setSubject(subject)
-          .addClaims(it.toMap())
-          .setExpiration(Date(System.currentTimeMillis() + expiryTime.toMillis()))
-          .signWith(keyPair.private, SignatureAlgorithm.RS256)
+          .id(jwtId)
+          .subject(subject)
+          .claims(it.toMap())
+          .expiration(Date(System.currentTimeMillis() + expiryTime.toMillis()))
+          .signWith(keyPair.private, Jwts.SIG.RS256)
           .compact()
       }
 }

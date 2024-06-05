7aac3c72fb913c6207722aca83736d7c6977c4a9
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2024-06-05 15:26:01 +0100

    PLT-275 Get CRS appointments from Interventions API - with env. variable enabled (#392)
    * PLT-275 Get CRS appointments from Interventions API - with env. variable included
    
    * PLT-275 Get CRS appointments from Interventions API - with env. variable enabled

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/external/InterventionsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/external/InterventionsApiService.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.external
 
+import org.hibernate.query.sqm.tree.SqmNode.log
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientException
 import org.springframework.web.reactive.function.client.WebClientResponseException
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.interventionsapi.CRSAppointmentsDTO
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.interventionsapi.ReferralDTO
 
 @Service
@@ -25,4 +28,19 @@ class InterventionsApiService(
       listOf(),
     )
     .block() ?: throw RuntimeException("Unexpected null returned from request.")
+
+  fun fetchCRSAppointments(crn: String): CRSAppointmentsDTO {
+    return interventionsWebClientCredentials.get()
+      .uri("/appointments-location/$crn")
+      .retrieve()
+      .bodyToMono(CRSAppointmentsDTO::class.java)
+      .onErrorReturn(
+        {
+          log.warn("Unexpected error from Intervention Service  API - ignoring but data will be missing from response!", it)
+          it is WebClientException
+        },
+        CRSAppointmentsDTO(crn = crn, referral = mutableListOf()),
+      )
+      .block() ?: throw RuntimeException("Unexpected null returned from request.")
+  }
 }

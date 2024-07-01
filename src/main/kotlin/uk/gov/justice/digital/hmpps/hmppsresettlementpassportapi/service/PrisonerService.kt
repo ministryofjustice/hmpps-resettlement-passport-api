8861a422339566fe7d066e630571b7e5964e3915
Matthew Kerry <157010890+mkerry-moj@users.noreply.github.com>
2024-07-01 15:22:37 +0100

    PLT-373 Create prisoner record on admission event (#403)
    * PLT-373 Create prisoner record on admission event
    
    When the record does not already exist
    
    * Add some indexes to prisoner and pathway_status

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/PrisonerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/PrisonerService.kt
@@ -275,14 +275,12 @@ class PrisonerService(
     setDisplayedReleaseDate(prisonerSearch)
 
     // Add initial pathway statuses if required
-    pathwayAndStatusService.addPrisonerAndInitialPathwayStatus(
+    val prisonerEntity = pathwayAndStatusService.getOrCreatePrisoner(
       nomsId,
       prisonerSearch.prisonId,
       prisonerSearch.displayReleaseDate,
     )
 
-    val prisonerEntity = prisonerRepository.findByNomsId(nomsId)
-      ?: throw ResourceNotFoundException("Unable to find prisoner $nomsId in database.")
     val prisonerImageDetailsList = prisonApiService.findPrisonerImageDetails(nomsId)
     var prisonerImage: PrisonerImage? = null
     prisonerImageDetailsList.forEach {
@@ -470,9 +468,7 @@ class PrisonerService(
     }
   }
 
-  fun getPrisonerEntity(nomsId: String): PrisonerEntity {
-    return prisonerRepository.findByNomsId(nomsId) ?: throw ResourceNotFoundException("Unable to find prisoner $nomsId in database.")
-  }
+  fun getPrisonerEntity(nomsId: String): PrisonerEntity = prisonerRepository.findByNomsId(nomsId) ?: throw ResourceNotFoundException("Unable to find prisoner $nomsId in database.")
 }
 
 private data class AssessmentRequiredResult(

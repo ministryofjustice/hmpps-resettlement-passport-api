dd9b796a1106ddf730bc951ca4cf8a16e97a72e6
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-07-04 12:07:14 +0100

    PLT-399 Incomplete code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/DocumentsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/DocumentsRepository.kt
@@ -10,4 +10,5 @@ interface DocumentsRepository : JpaRepository<DocumentsEntity, Long> {
   fun findAllByPrisoner(prisoner: PrisonerEntity): DocumentsEntity?
 
   fun findByPrisonerAndDocumentKey(prisoner: PrisonerEntity, documentKey: String): DocumentsEntity?
+
 }

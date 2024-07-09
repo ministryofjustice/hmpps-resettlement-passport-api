31dfc0247791ae96fba7ebba87040baf5223a7de
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-07-09 14:27:51 +0100

    PLT-399 Upload and Scan Document before storing - test unit included

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/IntegrationTestBase.kt
@@ -8,11 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.cache.CacheManager
+import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpHeaders
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlMergeMode
 import org.springframework.test.web.reactive.server.WebTestClient
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config.HmppsS3Properties
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.helpers.JwtAuthHelper
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.helpers.TestBase
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.wiremock.AllocationManagerApiMockServer
@@ -43,6 +50,12 @@ abstract class IntegrationTestBase : TestBase() {
 
   @Autowired
   lateinit var cacheManager: CacheManager
+
+  @Autowired
+  private lateinit var hmppsS3Properties: HmppsS3Properties
+
+  @Autowired
+  protected lateinit var s3Client: S3Client
 
   @BeforeEach
   fun beforeEach() {
@@ -137,6 +150,30 @@ abstract class IntegrationTestBase : TestBase() {
     scopes: List<String> = listOf(),
     authSource: String = "none",
   ): (HttpHeaders) -> Unit = jwtAuthHelper.setAuthorisation(user, roles, scopes, authSource)
+
+  internal fun bucketName() = hmppsS3Properties.buckets["document-management"]!!.bucketName
+
+  internal fun putDocumentInS3(documentKey: String, fileResourcePath: String): ByteArray {
+    val request = PutObjectRequest.builder()
+      .bucket(bucketName())
+      .key(documentKey)
+      .build()
+    val fileBytes = ClassPathResource(fileResourcePath).contentAsByteArray
+    s3Client.putObject(request, RequestBody.fromBytes(fileBytes))
+    return fileBytes
+  }
+
+  internal fun deleteAllDocumentsInS3() {
+    val listObjectsResponse = s3Client.listObjectsV2(ListObjectsV2Request.builder().bucket(bucketName()).build())
+
+    for (s3Object in listObjectsResponse.contents()) {
+      val request = DeleteObjectRequest.builder()
+        .bucket(bucketName())
+        .key(s3Object.key())
+        .build()
+      s3Client.deleteObject(request)
+    }
+  }
 }
 
 fun readFile(file: String): String = Resources.getResource(file).readText()

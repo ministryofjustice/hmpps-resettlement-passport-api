d99529fb64aedf7d872f3fdee2b688cac26f5a1b
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-07-09 14:27:51 +0100

    PLT-399 Upload and Scan Document before storing - test unit included

--- src/test/resources/testdata/sql/clear-all-data.sql
+++ src/test/resources/testdata/sql/clear-all-data.sql
@@ -1,3 +1,5 @@
+DELETE from document_location;
+ALTER SEQUENCE document_location_id_seq RESTART WITH 1;
 DELETE from licence_conditions_change_audit;
 ALTER SEQUENCE licence_conditions_change_audit_id_seq RESTART WITH 1;
 DELETE from person_on_probation_user_otp;

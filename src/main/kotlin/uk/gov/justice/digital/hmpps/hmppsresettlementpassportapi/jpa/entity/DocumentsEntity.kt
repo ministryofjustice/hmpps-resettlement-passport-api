27a55cdf0c7946ac4092d33fe296b51a542817e3
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-07-09 14:22:42 +0100

    Add stub conversion for tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/entity/DocumentsEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/entity/DocumentsEntity.kt
@@ -1,13 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity
 
-import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import java.time.LocalDateTime
 import java.util.UUID
@@ -19,15 +16,13 @@ class DocumentsEntity(
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   val id: Long? = null,
 
-  @ManyToOne(cascade = [CascadeType.MERGE])
-  @JoinColumn(name = "prisoner_id", referencedColumnName = "id")
-  val prisoner: PrisonerEntity,
+  val prisonerId: Long,
 
   @Column(name = "original_document_key")
   val originalDocumentKey: String,
 
   @Column(name = "html_document_key")
-  val htmlDocumentKey: UUID,
+  val htmlDocumentKey: UUID?,
 
   @Column(name = "creation_date")
   val creationDate: LocalDateTime,
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/LibreOfficeDocumentConversionServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/LibreOfficeDocumentConversionServiceTest.kt
@@ -3,26 +3,27 @@ package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service
 import com.google.common.io.Resources
 import io.mockk.every
 import io.mockk.mockk
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.mock.web.MockMultipartFile
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.model.PutObjectResponse
-import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.DocumentsRepository
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.function.Consumer
 
-class DocumentConversionServiceTest {
+@Disabled("Just used to quickly very libreoffice is working locally")
+class LibreOfficeDocumentConversionServiceTest {
   @Test
   fun `converts a document`() {
     val tempDir = Files.createTempDirectory("temp").toFile()
     tempDir.deleteOnExit()
     val s3Client = mockk<S3Client>()
-    val documentConversionService = DocumentConversionService(
+    val documentConversionService = LibreOfficeDocumentConversionService(
       tempDir,
       s3Client,
-      mockk<DocumentsRepository>(),
+      "bucket",
     )
 
     val fileData = Resources.getResource("testdata/PD1_example.docx").openStream()

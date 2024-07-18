d66c8ecf5c95f8256cdd8be68f20c389fa7009c5
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2024-07-18 15:13:41 +0100

    PLT-430 Document Upload with Category and Validation Included (#446)
    * PLT-430 Document Upload with Category and Validation Included
    
    * PLT-430 Document Upload with Category and Validation Included - review fix
    
    * PLT-430 Document Upload with Category and Validation Included - review fix

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/DocumentCategory.kt
@@ -0,0 +1,5 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data
+
+enum class DocumentCategory(val displayName: String) {
+  LICENCE_CONDITIONS("Licence Conditions"),
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/entity/DocumentsEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/entity/DocumentsEntity.kt
@@ -2,10 +2,13 @@ package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.DocumentCategory
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -26,4 +29,10 @@ class DocumentsEntity(
 
   @Column(name = "creation_date")
   val creationDate: LocalDateTime,
+
+  @Enumerated(EnumType.STRING)
+  val category: DocumentCategory,
+
+  @Column(name = "original_document_file_name")
+  val originalDocumentFileName: String,
 )

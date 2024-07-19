9a6a96863224cf26b40a5e2d937abe4d71292d49
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-07-19 13:12:32 +0100

    PLT-426 Handle unique constraint violation
    On create new prisoner.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/PathwayAndStatusServiceIntegrationTest.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PrisonerEntity
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.PrisonerRepository
+
+class PathwayAndStatusServiceIntegrationTest : IntegrationTestBase() {
+
+  @Autowired
+  private lateinit var pathwayAndStatusService: PathwayAndStatusService
+
+  @Autowired
+  private lateinit var prisonerRepository: PrisonerRepository
+
+  @Test
+  fun `Loads prisoner when trying to create and getting a unique constraint violation`() {
+    val existingPrisoner = prisonerRepository.save(
+      PrisonerEntity(
+        nomsId = "A123",
+        crn = "CRN123",
+        prisonId = "P123",
+        releaseDate = null,
+      ),
+    )
+
+    val result = pathwayAndStatusService.createPrisoner(nomsId = "A123", resolvedCrn = "CRN123", prisonId = "P123", releaseDate = null)
+
+    assertThat(result).isEqualTo(existingPrisoner)
+  }
+}

8e7adbc862a2ab0929a42efbc7a062f5df9f0565
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-07-23 14:49:50 +0100

    PSFR-1561 Fix wording of case note summary question (#454)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/AccommodationV1ResettlementAssessmentAssessmentStrategyTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/AccommodationV1ResettlementAssessmentAssessmentStrategyTest.kt
@@ -461,7 +461,7 @@ class AccommodationV1ResettlementAssessmentAssessmentStrategyTest : BaseResettle
           ResettlementAssessmentQuestionAndAnswer(
             question = ResettlementAssessmentQuestion(
               id = "CASE_NOTE_SUMMARY",
-              title = "Add a case note summary",
+              title = "Case note summary",
               subTitle = "This will be displayed as a case note in both DPS and nDelius",
               type = TypeOfQuestion.LONG_TEXT,
             ),
@@ -720,7 +720,7 @@ class AccommodationV1ResettlementAssessmentAssessmentStrategyTest : BaseResettle
         ResettlementAssessmentQuestionAndAnswer(
           ResettlementAssessmentQuestion(
             id = "CASE_NOTE_SUMMARY",
-            title = "Add a case note summary",
+            title = "Case note summary",
             subTitle = "This will be displayed as a case note in both DPS and nDelius",
             type = TypeOfQuestion.LONG_TEXT,
             options = null,
@@ -1608,7 +1608,7 @@ class AccommodationV1ResettlementAssessmentAssessmentStrategyTest : BaseResettle
           ),
           AssessmentConfigQuestion(
             id = "CASE_NOTE_SUMMARY",
-            title = "Add a case note summary",
+            title = "Case note summary",
             subTitle = "This will be displayed as a case note in both DPS and nDelius",
             type = TypeOfQuestion.LONG_TEXT,
             options = null,

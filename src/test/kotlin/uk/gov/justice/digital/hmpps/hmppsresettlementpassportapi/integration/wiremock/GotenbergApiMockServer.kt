1fc9b1eec70a3ea8061a0e14d59df8d51b630106
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-07-25 15:51:39 +0100

    PLT-435 Gotenberg Integration for the PDF Conversion in the document upload - removed unused

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/wiremock/GotenbergApiMockServer.kt
+++
@@ -1,5 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.wiremock
-
-import com.github.tomakehurst.wiremock.client.WireMock.get
-
-class GotenbergApiMockServer : WireMockServerBase(9107)

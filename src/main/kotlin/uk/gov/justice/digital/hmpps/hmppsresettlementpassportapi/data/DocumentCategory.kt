1332b836034b8f804e5562804548d4019cfc18b4
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-07-29 10:56:22 +0100

    PLT-435 Gotenberg Integration for the PDF Conversion changes - review fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/DocumentCategory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/DocumentCategory.kt
@@ -1,11 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data
 
-import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.DocumentsEntity
-
 enum class DocumentCategory(val displayName: String) {
   LICENCE_CONDITIONS("Licence Conditions"),
 }
-
-data class DocumentResponse(
-  val value: DocumentsEntity,
-)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/DocumentResponse.kt
@@ -0,0 +1,7 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data
+
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.DocumentsEntity
+
+data class DocumentResponse(
+  val value: DocumentsEntity,
+)

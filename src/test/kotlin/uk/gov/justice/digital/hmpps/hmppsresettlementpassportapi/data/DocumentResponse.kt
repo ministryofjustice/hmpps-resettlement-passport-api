e737e3ac10d1fe66014b1b80ab73f01ab8d05f64
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2024-07-29 11:53:58 +0100

    PLT-435 Gotenberg Integration for the PDF Conversion changes - review fix (#464)

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/DocumentResponse.kt
@@ -0,0 +1,7 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data
+
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.DocumentsEntity
+
+class DocumentResponse(
+  val value: DocumentsEntity,
+)

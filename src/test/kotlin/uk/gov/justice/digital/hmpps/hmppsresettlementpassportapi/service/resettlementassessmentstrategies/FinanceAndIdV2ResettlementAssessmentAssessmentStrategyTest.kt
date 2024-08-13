a429e9fe872b350df029c218ad533f8df947ce12
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2024-08-13 16:07:58 +0100

    fix trailing commas

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/FinanceAndIdV2ResettlementAssessmentAssessmentStrategyTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/FinanceAndIdV2ResettlementAssessmentAssessmentStrategyTest.kt
@@ -286,7 +286,7 @@ class FinanceAndIdV2ResettlementAssessmentAssessmentStrategyTest : BaseResettlem
               subTitle = "Include any relevant information about why you have chosen that resettlement status. Do not include any special category data. This information will be displayed in PSFR on the overview tab and the finance and ID tab.",
               type = TypeOfQuestion.LONG_TEXT,
               detailsTitle = "Help with special category data",
-              detailsContent = "Special category data includes any personal data concerning someone's health, sex life or sexual orientation. Or any personal data revealing someone's racial or ethnic origin, religious or philosophical beliefs or trade union membership."
+              detailsContent = "Special category data includes any personal data concerning someone's health, sex life or sexual orientation. Or any personal data revealing someone's racial or ethnic origin, religious or philosophical beliefs or trade union membership.",
             ),
             originalPageId = "ASSESSMENT_SUMMARY",
           ),

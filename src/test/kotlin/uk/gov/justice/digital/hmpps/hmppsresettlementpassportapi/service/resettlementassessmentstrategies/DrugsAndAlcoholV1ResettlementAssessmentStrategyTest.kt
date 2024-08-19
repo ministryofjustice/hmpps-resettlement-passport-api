4dc90db14b4b611094237317057480b80cfe0bf2
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2024-08-19 08:13:48 +0100

    PSFR-1603: version 2 pathway

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/DrugsAndAlcoholV1ResettlementAssessmentStrategyTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/DrugsAndAlcoholV1ResettlementAssessmentStrategyTest.kt
@@ -17,7 +17,7 @@ import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.helpers.yesNoOp
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.ResettlementAssessmentType
 import java.util.stream.Stream
 
-class DrugsAndAlcoholResettlementAssessmentAssessmentStrategyTest : BaseResettlementAssessmentStrategyTest(Pathway.DRUGS_AND_ALCOHOL) {
+class DrugsAndAlcoholV1ResettlementAssessmentStrategyTest : BaseResettlementAssessmentStrategyTest(Pathway.DRUGS_AND_ALCOHOL) {
 
   @ParameterizedTest
   @MethodSource("test next page function flow - no existing assessment data")
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/resettlementassessmentstrategies/DrugsAndAlcoholV2ResettlementAssessmentStrategyTest.kt
@@ -0,0 +1,204 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.resettlementassessmentstrategies
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.Pathway
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.ResettlementAssessmentOption
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.ResettlementAssessmentQuestion
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.ResettlementAssessmentQuestionAndAnswer
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.ResettlementAssessmentRequest
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.ResettlementAssessmentRequestQuestionAndAnswer
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.ResettlementAssessmentResponsePage
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.resettlementassessment.TypeOfQuestion
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.helpers.yesNoOptions
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.ResettlementAssessmentType
+import java.util.stream.Stream
+
+class DrugsAndAlcoholV2ResettlementAssessmentStrategyTest : BaseResettlementAssessmentStrategyTest(Pathway.DRUGS_AND_ALCOHOL) {
+
+  @ParameterizedTest(name = "{1} -> {2}")
+  @MethodSource("test next page function flow - no existing assessment data")
+  fun `test next page function flow - no existing assessment`(
+    questionsAndAnswers: List<ResettlementAssessmentRequestQuestionAndAnswer<*>>,
+    currentPage: String?,
+    expectedPage: String,
+  ) {
+    val nomsId = "123"
+    setUpMocks(nomsId, false)
+
+    val assessment = ResettlementAssessmentRequest(
+      questionsAndAnswers = questionsAndAnswers,
+    )
+    val nextPage = resettlementAssessmentStrategy.getNextPageId(
+      assessment = assessment,
+      nomsId = nomsId,
+      pathway = Pathway.DRUGS_AND_ALCOHOL,
+      assessmentType = ResettlementAssessmentType.BCST2,
+      currentPage = currentPage,
+      version = 2,
+    )
+    Assertions.assertEquals(expectedPage, nextPage)
+  }
+
+  private fun `test next page function flow - no existing assessment data`() = Stream.of(
+    // Start of flow - send null current page to get first page
+    Arguments.of(
+      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(),
+      null,
+      "DRUGS_AND_ALCOHOL_REPORT",
+    ),
+    // Any answer to DRUGS_AND_ALCOHOL_REPORT, go to SUPPORT_REQUIREMENTS
+    Arguments.of(
+      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(),
+      "DRUGS_AND_ALCOHOL_REPORT",
+      "SUPPORT_REQUIREMENTS",
+    ),
+    // Any answer to SUPPORT_REQUIREMENTS, go to ASSESSMENT_SUMMARY
+    Arguments.of(
+      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(),
+      "SUPPORT_REQUIREMENTS",
+      "ASSESSMENT_SUMMARY",
+    ),
+    // Any answer to ASSESSMENT_SUMMARY, go to CHECK_ANSWERS
+    Arguments.of(
+      listOf<ResettlementAssessmentRequestQuestionAndAnswer<*>>(),
+      "ASSESSMENT_SUMMARY",
+      "CHECK_ANSWERS",
+    ),
+  )
+
+  @ParameterizedTest(name = "{0} page")
+  @MethodSource("test get page from Id - no existing assessment data")
+  fun `test get page from Id - no existing assessment`(pageIdInput: String, expectedPage: ResettlementAssessmentResponsePage) {
+    val nomsId = "123"
+    setUpMocks("123", false)
+
+    val page = resettlementAssessmentStrategy.getPageFromId(
+      nomsId = nomsId,
+      pathway = Pathway.DRUGS_AND_ALCOHOL,
+      assessmentType = ResettlementAssessmentType.BCST2,
+      pageId = pageIdInput,
+      version = 2,
+    )
+    Assertions.assertEquals(expectedPage, page)
+  }
+
+  private fun `test get page from Id - no existing assessment data`() = Stream.of(
+    Arguments.of(
+      "DRUGS_AND_ALCOHOL_REPORT",
+      ResettlementAssessmentResponsePage(
+        id = "DRUGS_AND_ALCOHOL_REPORT",
+        title = "Drugs and alcohol report",
+        questionsAndAnswers = listOf(
+          ResettlementAssessmentQuestionAndAnswer(
+            question = ResettlementAssessmentQuestion(
+              id = "DRUG_MISUSE_ISSUES",
+              title = "Does the person in prison have any previous or current drug misuse issues?",
+              subTitle = null,
+              type = TypeOfQuestion.RADIO,
+              options = yesNoOptions,
+            ),
+            originalPageId = "DRUGS_AND_ALCOHOL_REPORT",
+          ),
+          ResettlementAssessmentQuestionAndAnswer(
+            question = ResettlementAssessmentQuestion(
+              id = "ALCOHOL_MISUSE_ISSUES",
+              title = "Does the person in prison have any previous or current alcohol misuse issues?",
+              subTitle = null,
+              type = TypeOfQuestion.RADIO,
+              options = yesNoOptions,
+            ),
+            originalPageId = "DRUGS_AND_ALCOHOL_REPORT",
+          ),
+        )
+      ),
+    ),
+    Arguments.of(
+      "SUPPORT_REQUIREMENTS",
+      ResettlementAssessmentResponsePage(
+        id = "SUPPORT_REQUIREMENTS",
+        questionsAndAnswers = listOf(
+          ResettlementAssessmentQuestionAndAnswer(
+            question = ResettlementAssessmentQuestion(
+              id = "SUPPORT_REQUIREMENTS",
+              title = "Support needs",
+              subTitle = "Select any needs you have identified that could be met by prison or probation staff.",
+              type = TypeOfQuestion.CHECKBOX,
+              options = listOf(
+                ResettlementAssessmentOption(
+                  id = "SUPPORT_FOR_DRUG_ISSUES",
+                  displayText = "Support from drug and alcohol team for drug issues",
+                ),
+                ResettlementAssessmentOption(
+                  id = "SUPPORT_FOR_ALCOHOL_ISSUES",
+                  displayText = "Support from drug and alcohol team for alcohol issues",
+                ),
+                ResettlementAssessmentOption(
+                  id = "CONTACT_SUPPORT_SERVICES_OUTSIDE_PRISON",
+                  displayText = "Contact any support services they have used outside of prison",
+                ),
+                ResettlementAssessmentOption(
+                  id = "NO_SUPPORT_NEEDS",
+                  displayText = "No support needs identified",
+                  exclusive = true,
+                ),
+              ),
+            ),
+            originalPageId = "SUPPORT_REQUIREMENTS",
+          ),
+        ),
+      ),
+    ),
+    Arguments.of(
+      "ASSESSMENT_SUMMARY",
+      ResettlementAssessmentResponsePage(
+        id = "ASSESSMENT_SUMMARY",
+        title = "Drugs and alcohol report summary",
+        questionsAndAnswers = listOf(
+          ResettlementAssessmentQuestionAndAnswer(
+            question = ResettlementAssessmentQuestion(
+              id = "SUPPORT_NEEDS",
+              title = "Drugs and alcohol resettlement status",
+              subTitle = "Select one option.",
+              type = TypeOfQuestion.RADIO,
+              options = listOf(
+                ResettlementAssessmentOption(
+                  id = "SUPPORT_REQUIRED",
+                  displayText = "Support required",
+                  description = "a need for support has been identified and is accepted",
+                ),
+                ResettlementAssessmentOption(id = "SUPPORT_NOT_REQUIRED", displayText = "Support not required", description = "no need was identified"),
+                ResettlementAssessmentOption(
+                  id = "SUPPORT_DECLINED",
+                  displayText = "Support declined",
+                  description = "a need has been identified but support is declined",
+                ),
+              ),
+            ),
+            originalPageId = "ASSESSMENT_SUMMARY",
+          ),
+          ResettlementAssessmentQuestionAndAnswer(
+            question = ResettlementAssessmentQuestion(
+              id = "CASE_NOTE_SUMMARY",
+              title = "Case note",
+              subTitle = "Include any relevant information about why you have chosen that resettlement status. This information will be only displayed in PSfR. Do not include any information that could identify anyone other than the person in prison, or any  special category data.",
+              type = TypeOfQuestion.LONG_TEXT,
+              detailsTitle = "Help with special category data",
+              detailsContent = "Special category data includes any personal data concerning someone's health, sex life or sexual orientation. Or any personal data revealing someone's racial or ethnic origin, religious or philosophical beliefs or trade union membership.",
+            ),
+            originalPageId = "ASSESSMENT_SUMMARY",
+          ),
+        ),
+      ),
+    ),
+    Arguments.of(
+      "CHECK_ANSWERS",
+      ResettlementAssessmentResponsePage(
+        id = "CHECK_ANSWERS",
+        questionsAndAnswers = listOf(),
+      ),
+    ),
+  )
+}

052909cce8d130c5bcbfb660e4cb6e46798321fe
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-09-06 12:49:01 +0100

    PSFR-1401 External API failure metric recorded

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/external/PrisonerSearchApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/external/PrisonerSearchApiService.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.external
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -12,34 +14,59 @@ import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonersa
 @Service
 class PrisonerSearchApiService(
   private val prisonerSearchWebClientClientCredentials: WebClient,
+  private val registry: MeterRegistry,
 ) {
+  private val componentName = "PRISONER-SEARCH"
 
   @Cacheable("prisoner-search-api-find-prisoners-by-prison-id")
   fun findPrisonersByPrisonId(prisonId: String): List<PrisonersSearch> {
     val listToReturn = mutableListOf<PrisonersSearch>()
+    try {
+      var page = 0
+      do {
+        val data = prisonerSearchWebClientClientCredentials.get()
+          .uri(
+            "/prison/{prisonId}/prisoners?size={size}&page={page}&sort={sort}",
+            mapOf(
+              "prisonId" to prisonId,
+              // NB: API allows up 3,000 results per page
+              "size" to 500,
+              "page" to page,
+              "sort" to "prisonerNumber",
+            ),
+          )
+          .retrieve()
+          .onStatus({ it.is5xxServerError }, {
+            registry.counter("external_api_5xx_fail", Tags.of("service", componentName)).increment()
+            throw Exception(it.toString())
+          })
+          .onStatus(
+            { it.is4xxClientError },
+          ) {
+            registry.counter("external_api_4xx_fail", Tags.of("service", componentName)).increment()
+            if (it.statusCode() == HttpStatus.NOT_FOUND) {
+              throw ResourceNotFoundException("PrisonId $prisonId not found")
+            } else {
+              throw Exception(it.toString())
+            }
+          }
+          .onStatus(
+            { !it.is2xxSuccessful },
+          ) {
+            registry.counter("external_api_fail", Tags.of("service", componentName)).increment()
+            throw Exception(it.toString())
+          }
 
-    var page = 0
-    do {
-      val data = prisonerSearchWebClientClientCredentials.get()
-        .uri(
-          "/prison/{prisonId}/prisoners?size={size}&page={page}&sort={sort}",
-          mapOf(
-            "prisonId" to prisonId,
-            // NB: API allows up 3,000 results per page
-            "size" to 500,
-            "page" to page,
-            "sort" to "prisonerNumber",
-          ),
-        )
-        .retrieve()
-        .onStatus({ it == HttpStatus.NOT_FOUND }, { throw ResourceNotFoundException("PrisonId $prisonId not found") })
-
-      val pageOfData = data.bodyToMono<PrisonersSearchList>().block()
-      if (pageOfData != null) {
-        listToReturn.addAll(pageOfData.content!!)
-      }
-      page += 1
-    } while (!pageOfData?.last!!)
+        val pageOfData = data.bodyToMono<PrisonersSearchList>().block()
+        if (pageOfData != null) {
+          listToReturn.addAll(pageOfData.content!!)
+        }
+        page += 1
+      } while (!pageOfData?.last!!)
+    } catch (e: Exception) {
+      registry.counter("external_api_fail", Tags.of("service", componentName)).increment()
+      throw e
+    }
     return listToReturn
   }
 

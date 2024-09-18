cd10e375c8dbf0b5f05076f0b80e2ed7382e2905
Simon Turner <simon.turner@hippodigital.co.uk>
2024-09-18 11:19:27 +0100

    PSFR-1718 - Try / Catch block around call

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/audit/AuditService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/audit/AuditService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.audit
 
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebInputException
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.getClaimFromJWTToken
@@ -8,18 +9,24 @@ import uk.gov.justice.hmpps.sqs.audit.HmppsAuditService
 
 @Component
 class AuditService(private val auditService: HmppsAuditService) {
+  private val log = LoggerFactory.getLogger(this::class.java)
+
   fun audit(what: AuditAction, nomsId: String, auth: String, vararg details: String) {
     val userName = getClaimFromJWTToken(auth, "sub") ?: throw ServerWebInputException("Cannot get name from auth token")
 
     runBlocking {
-      auditService.publishEvent(
-        what = what.name,
-        who = userName,
-        subjectId = nomsId,
-        subjectType = "PRISONER_ID",
-        service = "hmpps-resettlement-passport-api",
-        details = details.firstOrNull(),
-      )
+      try {
+        auditService.publishEvent(
+          what = what.name,
+          who = userName,
+          subjectId = nomsId,
+          subjectType = "PRISONER_ID",
+          service = "hmpps-resettlement-passport-api",
+          details = details.firstOrNull(),
+        )
+      } catch (e: Exception) {
+        log.error("Unable to publish audit event", e)
+      }
     }
   }
 }

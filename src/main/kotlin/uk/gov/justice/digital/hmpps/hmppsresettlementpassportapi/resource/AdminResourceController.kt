24b9c4f7122899e81754dde559431b97f3871023
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-12-09 09:28:43 +0000

    PSFR-1945 Add cron job to send metrics to app insights (#598)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/AdminResourceController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/AdminResourceController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.AdminService
@@ -25,6 +26,22 @@ class AdminResourceController(private val adminService: AdminService) {
   )
   fun retryFailedDeliusCaseNotes(): ResponseEntity<Void> {
     adminService.retryFailedDeliusCaseNotes()
+    return ResponseEntity.ok().build()
+  }
+
+  @ProtectedByIngress
+  @PostMapping("/send-metrics", produces = [MediaType.APPLICATION_JSON_VALUE])
+  @Operation(summary = "Send metrics to app insights")
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successful Operation",
+      ),
+    ],
+  )
+  fun sendMetricsToAppInsights(): ResponseEntity<Void> {
+    adminService.sendMetricsToAppInsights()
     return ResponseEntity.ok().build()
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/AdminServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/AdminServiceTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service
 
+import com.microsoft.applicationinsights.TelemetryClient
 import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
@@ -11,9 +12,14 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data.prisonersapi.PrisonersSearch
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.CaseNoteRetryEntity
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.CaseNoteRetryRepository
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.CaseNoteRetryService.Companion.MAX_RETRIES
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.external.PrisonerSearchApiService
 import java.time.LocalDateTime
 
 @ExtendWith(MockitoExtension::class)
@@ -25,6 +31,15 @@ class AdminServiceTest {
 
   @Mock
   private lateinit var caseNoteRetryService: CaseNoteRetryService
+
+  @Mock
+  private lateinit var telemetryClient: TelemetryClient
+
+  @Mock
+  private lateinit var caseAllocationService: CaseAllocationService
+
+  @Mock
+  private lateinit var prisonerSearchApiService: PrisonerSearchApiService
 
   companion object {
 
@@ -46,7 +61,7 @@ class AdminServiceTest {
 
   @BeforeEach
   fun beforeEach() {
-    adminService = AdminService(caseNoteRetryService, caseNoteRetryRepository)
+    adminService = AdminService(caseNoteRetryService, caseNoteRetryRepository, telemetryClient, caseAllocationService, prisonerSearchApiService)
   }
 
   @Test
@@ -59,5 +74,35 @@ class AdminServiceTest {
     caseNotesToRetry.forEach {
       Mockito.verify(caseNoteRetryService).processDeliusCaseNote(it)
     }
+  }
+
+  @Test
+  fun `test sendMetricsToAppInsights`() {
+    whenever(caseAllocationService.getPrisonsWithCaseAllocations()).thenReturn(listOf("AAI", "BBI", "CCI", "DDI"))
+
+    whenever(caseAllocationService.getNumberOfAssignedPrisoners("AAI")).thenReturn(5)
+    whenever(caseAllocationService.getNumberOfAssignedPrisoners("BBI")).thenReturn(10)
+    whenever(caseAllocationService.getNumberOfAssignedPrisoners("CCI")).thenReturn(7)
+    whenever(caseAllocationService.getNumberOfAssignedPrisoners("DDI")).thenReturn(0)
+
+    whenever(prisonerSearchApiService.findPrisonersByPrisonId("AAI")).thenReturn(getMocks(10))
+    whenever(prisonerSearchApiService.findPrisonersByPrisonId("BBI")).thenReturn(getMocks(35))
+    whenever(prisonerSearchApiService.findPrisonersByPrisonId("CCI")).thenReturn(getMocks(102))
+    whenever(prisonerSearchApiService.findPrisonersByPrisonId("DDI")).thenThrow(RuntimeException("Something went wrong"))
+
+    adminService.sendMetricsToAppInsights()
+
+    verify(telemetryClient).trackMetric("case_allocation_assigned_prisoners_percentage", 50.0, null, null, null, null, mapOf("prisonId" to "AAI", "numberOfPrisonersAssigned" to "5", "totalNumberOfPrisoners" to "10"))
+    verify(telemetryClient).trackMetric("case_allocation_assigned_prisoners_percentage", 28.57142857142857, null, null, null, null, mapOf("prisonId" to "BBI", "numberOfPrisonersAssigned" to "10", "totalNumberOfPrisoners" to "35"))
+    verify(telemetryClient).trackMetric("case_allocation_assigned_prisoners_percentage", 6.862745098039216, null, null, null, null, mapOf("prisonId" to "CCI", "numberOfPrisonersAssigned" to "7", "totalNumberOfPrisoners" to "102"))
+    verifyNoMoreInteractions(telemetryClient)
+  }
+
+  private fun getMocks(num: Int): List<PrisonersSearch> {
+    val list = mutableListOf<PrisonersSearch>()
+    repeat(num) {
+      list.add(Mockito.mock(PrisonersSearch::class.java))
+    }
+    return list
   }
 }

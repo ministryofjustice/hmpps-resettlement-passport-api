914bb0bb02e594b27333c128aa59405a1f7046da
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-12-10 14:35:16 +0000

    fix formatting

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/PrisonerRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/PrisonerRepositoryTest.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PrisonerEntity
@@ -24,15 +22,17 @@ class PrisonerRepositoryTest : RepositoryTestBase() {
 
   @Test
   fun `test findDistinctPrisonIds`() {
-    prisonerRepository.saveAll(listOf(
-      PrisonerEntity(null, "NOM0001", LocalDateTime.now(), "crn1", "AA1"),
-      PrisonerEntity(null, "NOM0002", LocalDateTime.now(), "crn2", "AA3"),
-      PrisonerEntity(null, "NOM0003", LocalDateTime.now(), "crn3", "AA2"),
-      PrisonerEntity(null, "NOM0004", LocalDateTime.now(), "crn4", "AA2"),
-      PrisonerEntity(null, "NOM0005", LocalDateTime.now(), "crn5", "AA2"),
-      PrisonerEntity(null, "NOM0006", LocalDateTime.now(), "crn6", "AA1"),
-      PrisonerEntity(null, "NOM0007", LocalDateTime.now(), "crn7", "AA4"),
-    ))
+    prisonerRepository.saveAll(
+      listOf(
+        PrisonerEntity(null, "NOM0001", LocalDateTime.now(), "crn1", "AA1"),
+        PrisonerEntity(null, "NOM0002", LocalDateTime.now(), "crn2", "AA3"),
+        PrisonerEntity(null, "NOM0003", LocalDateTime.now(), "crn3", "AA2"),
+        PrisonerEntity(null, "NOM0004", LocalDateTime.now(), "crn4", "AA2"),
+        PrisonerEntity(null, "NOM0005", LocalDateTime.now(), "crn5", "AA2"),
+        PrisonerEntity(null, "NOM0006", LocalDateTime.now(), "crn6", "AA1"),
+        PrisonerEntity(null, "NOM0007", LocalDateTime.now(), "crn7", "AA4"),
+      ),
+    )
 
     assertThat(prisonerRepository.findDistinctPrisonIds()).isEqualTo(listOf("AA1", "AA2", "AA3", "AA4"))
   }

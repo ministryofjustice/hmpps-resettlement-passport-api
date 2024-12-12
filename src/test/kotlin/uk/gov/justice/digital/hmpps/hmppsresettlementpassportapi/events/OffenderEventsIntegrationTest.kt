442e01a33b083c131a76f938eefd253b1b52d215
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-12-12 10:33:36 +0000

    PSFR-1957 Set prisoners to OUT on release event (#603)
    * PSFR-1957 Set prisoners to OUT on release event
    
    * fix logging
    
    * fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/events/OffenderEventsIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/events/OffenderEventsIntegrationTest.kt
@@ -10,9 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.jdbc.Sql
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.integration.readFile
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.PrisonerEntity
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.PrisonerRepository
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.MissingQueueException
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.util.*
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 
@@ -22,7 +26,7 @@ class OffenderEventsIntegrationTest : IntegrationTestBase() {
   private lateinit var hmppsQueueService: HmppsQueueService
 
   @Autowired
-  private lateinit var repository: OffenderEventRepository
+  private lateinit var offenderEventRepository: OffenderEventRepository
 
   @Autowired
   private lateinit var prisonerRepository: PrisonerRepository
@@ -45,7 +49,7 @@ class OffenderEventsIntegrationTest : IntegrationTestBase() {
     }.await()
 
     await.atMost(2.seconds.toJavaDuration()).untilAsserted {
-      val saved = repository.findAllByPrisonerId(1)
+      val saved = offenderEventRepository.findAllByPrisonerId(1)
       assertThat(saved).hasSize(1)
       assertThat(saved[0].reason).isEqualTo(MovementReasonType.RECALL)
     }
@@ -62,7 +66,7 @@ class OffenderEventsIntegrationTest : IntegrationTestBase() {
     }.await()
 
     await.atMost(2.seconds.toJavaDuration()).untilAsserted {
-      val saved = repository.findAllByPrisonerId(1)
+      val saved = offenderEventRepository.findAllByPrisonerId(1)
       assertThat(saved).hasSize(1)
       assertThat(saved[0].reason).isNull()
     }
@@ -78,9 +82,28 @@ class OffenderEventsIntegrationTest : IntegrationTestBase() {
     await.atMost(2.seconds.toJavaDuration()).untilAsserted {
       val createdPrisoner = prisonerRepository.findByNomsId("A4092EA")
       assertThat(createdPrisoner).isNotNull()
-      val saved = repository.findAllByPrisonerId(createdPrisoner?.id!!)
+      val saved = offenderEventRepository.findAllByPrisonerId(createdPrisoner?.id!!)
       assertThat(saved).hasSize(1)
       assertThat(saved[0].reason).isNull()
+    }
+  }
+
+  @Sql("classpath:testdata/sql/seed-prisoners-for-events.sql")
+  @Test
+  fun `Release event is processed`() = runTest {
+    offenderEventsQueue.sqsClient.sendMessage { builder ->
+      builder.queueUrl(offenderEventsQueue.queueUrl)
+        .messageBody(readFile("testdata/events/release-event.json"))
+    }.await()
+
+    await.atMost(2.seconds.toJavaDuration()).untilAsserted {
+      val savedOffenderEvent = offenderEventRepository.findAllByPrisonerId(1)
+      assertThat(savedOffenderEvent).hasSize(1)
+      assertThat(savedOffenderEvent[0]).usingRecursiveComparison().ignoringFields("id", "creationDate").isEqualTo(OffenderEventEntity(id = UUID.randomUUID(), prisonerId = 1, nomsId = "A4092EA", type = OffenderEventType.PRISON_RELEASE, occurredAt = ZonedDateTime.parse("2024-12-11T15:43:20+00:00"), reason = null, reasonCode = "NCS", creationDate = LocalDateTime.parse("2024-12-11T16:47:12.426329")))
+
+      val savedPrisoner = prisonerRepository.findAll()
+      assertThat(savedPrisoner).hasSize(1)
+      assertThat(savedPrisoner[0]).isEqualTo(PrisonerEntity(1, "A4092EA", LocalDateTime.parse("2023-08-16T12:21:38.709"), "123", "OUT"))
     }
   }
 }

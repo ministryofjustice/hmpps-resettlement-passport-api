5a3a8e3edae1556fc26dbb018d696bf7e7acb021
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2025-01-13 15:38:57 +0000

    fix tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/SupportNeedRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/jpa/repository/SupportNeedRepositoryTest.kt
@@ -17,11 +17,11 @@ class SupportNeedRepositoryTest : RepositoryTestBase() {
   @Sql("classpath:testdata/sql/seed-support-needs.sql") // TODO - remove this once RSP-1718 is done
   fun `test can find support needs`() {
     val expectedSupportNeeds = listOf(
-      SupportNeedEntity(id = 1, pathway = Pathway.ACCOMMODATION, section = "Section A", title = "Support Need 1", hidden = false, excludeFromCount = false, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-09-08T13:00"), isDeleted = false, deletedDate = null),
-      SupportNeedEntity(id = 2, pathway = Pathway.ACCOMMODATION, section = "Section A", title = "Support Need 2", hidden = false, excludeFromCount = false, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-09-08T13:00"), isDeleted = false, deletedDate = null),
-      SupportNeedEntity(id = 3, pathway = Pathway.ACCOMMODATION, section = "Section A", title = "Support Need 3", hidden = false, excludeFromCount = false, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-09-08T13:00"), isDeleted = true, deletedDate = LocalDateTime.parse("2024-09-08T14:00")),
-      SupportNeedEntity(id = 4, pathway = Pathway.ACCOMMODATION, section = "Section B", title = "Support Need 4", hidden = true, excludeFromCount = true, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-09-08T13:00"), isDeleted = false, deletedDate = null),
-      SupportNeedEntity(id = 5, pathway = Pathway.ACCOMMODATION, section = "Section B", title = "Other", hidden = false, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2024-09-08T13:00"), isDeleted = false, deletedDate = null),
+      SupportNeedEntity(id = 1, pathway = Pathway.ACCOMMODATION, section = "Section A", title = "Support Need 1", hidden = false, excludeFromCount = false, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-02-08T12:00"), isDeleted = false, deletedDate = null),
+      SupportNeedEntity(id = 2, pathway = Pathway.ACCOMMODATION, section = "Section A", title = "Support Need 2", hidden = false, excludeFromCount = false, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-02-08T12:00"), isDeleted = false, deletedDate = null),
+      SupportNeedEntity(id = 3, pathway = Pathway.ACCOMMODATION, section = "Section A", title = "Support Need 3", hidden = false, excludeFromCount = false, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-02-08T12:00"), isDeleted = true, deletedDate = LocalDateTime.parse("2024-02-08T13:00")),
+      SupportNeedEntity(id = 4, pathway = Pathway.ACCOMMODATION, section = "Section B", title = "Support Need 4", hidden = true, excludeFromCount = true, allowOtherDetail = false, createdDate = LocalDateTime.parse("2024-02-08T12:00"), isDeleted = false, deletedDate = null),
+      SupportNeedEntity(id = 5, pathway = Pathway.ACCOMMODATION, section = "Section B", title = "Other", hidden = false, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2024-02-08T12:00"), isDeleted = false, deletedDate = null),
     )
 
     val supportNeedsFromDatabase = supportNeedRepository.findAll()

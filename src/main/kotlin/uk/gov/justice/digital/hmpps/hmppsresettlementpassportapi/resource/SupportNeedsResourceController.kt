1c04d8616e9c13b4760e0e045597592a182c7279
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2025-01-20 10:28:33 +0000

    RSP-1719 Add new support needs summary endpoint (#620)
    * RSP-1719 Add new support needs summary endpoint
    
    * fix tests

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/resource/SupportNeedsResourceController.kt
@@ -0,0 +1,57 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.SupportNeedsService
+
+@RestController
+@Validated
+@RequestMapping("/resettlement-passport/prisoner", produces = [MediaType.APPLICATION_JSON_VALUE])
+@PreAuthorize("hasRole('RESETTLEMENT_PASSPORT_EDIT')")
+class SupportNeedsResourceController(
+  private val supportNeedsService: SupportNeedsService,
+) {
+  @GetMapping("/{nomsId}/needs/summary")
+  @Operation(summary = "Get summary of support needs", description = "Get summary of support needs")
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successful Operation",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Prisoner not found in database",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  fun getSupportNeedsSummary(
+    @Schema(example = "AXXXS", required = true)
+    @PathVariable("nomsId")
+    @Parameter(required = true)
+    nomsId: String,
+  ) = supportNeedsService.getNeedsSummaryByNomsId(nomsId)
+}

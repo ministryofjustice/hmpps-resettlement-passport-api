36d005d9797fddbfff2e0967679bc1554f3dcd5f
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2025-01-31 15:18:43 +0000

    RSP-1721 Add POST endpoint for support needs

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/data/PrisonerNeedsRequest.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.data
+
+data class PrisonerNeedsRequest(
+  val needs: List<PrisonerNeedRequest>,
+)
+
+data class PrisonerNeedRequest(
+  val needId: Long,
+  val prisonerSupportNeedId: Long?,
+  val otherDesc: String?,
+  val text: String?,
+  val status: SupportNeedStatus?,
+  val isPrisonResponsible: Boolean?,
+  val isProbationResponsible: Boolean?,
+)

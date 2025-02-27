c8f7ef3135ca185775df7bf6c4471f7908ecd288
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2025-02-27 08:46:29 +0000

    fix linting

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/SupportNeedsLegacyProfileServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/service/SupportNeedsLegacyProfileServiceTest.kt
@@ -21,7 +21,6 @@ import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.entity.Supp
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.PrisonerRepository
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.PrisonerSupportNeedRepository
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.jpa.repository.SupportNeedRepository
-import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.service.AdminServiceTest.Companion
 import java.time.LocalDateTime
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -87,35 +86,43 @@ class SupportNeedsLegacyProfileServiceTest {
     mockkStatic(LocalDateTime::class)
     every { LocalDateTime.now() } returns fakeNow
 
-    whenever(supportNeedRepository.findAllBySectionAndDeletedIsFalse("Legacy support need")).thenReturn(listOf(
-      SupportNeedEntity(id = 101, pathway = Pathway.ACCOMMODATION, section = "Legacy support need", title = "Accommodation", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00:00")),
-      SupportNeedEntity(id = 102, pathway = Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, section = "Legacy support need", title = "Attitudes, thinking and behaviour", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00:00")),
-      SupportNeedEntity(id = 103, pathway = Pathway.HEALTH, section = "Legacy support need", title = "Health", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00:00")),
-    ))
+    whenever(supportNeedRepository.findAllBySectionAndDeletedIsFalse("Legacy support need")).thenReturn(
+      listOf(
+        SupportNeedEntity(id = 101, pathway = Pathway.ACCOMMODATION, section = "Legacy support need", title = "Accommodation", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00:00")),
+        SupportNeedEntity(id = 102, pathway = Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, section = "Legacy support need", title = "Attitudes, thinking and behaviour", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00:00")),
+        SupportNeedEntity(id = 103, pathway = Pathway.HEALTH, section = "Legacy support need", title = "Health", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00:00")),
+      ),
+    )
 
-    whenever(prisonerRepository.findAllBySupportNeedsLegacyProfileIsTrue()).thenReturn(listOf(
-      PrisonerEntity(id = 1, nomsId = "A1", prisonId = "ABC", supportNeedsLegacyProfile = true),
-      PrisonerEntity(id = 2, nomsId = "A2", prisonId = "ABC", supportNeedsLegacyProfile = true),
-      PrisonerEntity(id = 3, nomsId = "A3", prisonId = "ABC", supportNeedsLegacyProfile = true),
-    ))
+    whenever(prisonerRepository.findAllBySupportNeedsLegacyProfileIsTrue()).thenReturn(
+      listOf(
+        PrisonerEntity(id = 1, nomsId = "A1", prisonId = "ABC", supportNeedsLegacyProfile = true),
+        PrisonerEntity(id = 2, nomsId = "A2", prisonId = "ABC", supportNeedsLegacyProfile = true),
+        PrisonerEntity(id = 3, nomsId = "A3", prisonId = "ABC", supportNeedsLegacyProfile = true),
+      ),
+    )
 
     whenever(prisonerSupportNeedRepository.countPrisonerSupportNeedEntityByPrisonerIdAndDeletedIsFalse(1)).thenReturn(0)
     whenever(prisonerSupportNeedRepository.countPrisonerSupportNeedEntityByPrisonerIdAndDeletedIsFalse(2)).thenReturn(0)
     whenever(prisonerSupportNeedRepository.countPrisonerSupportNeedEntityByPrisonerIdAndDeletedIsFalse(3)).thenReturn(7)
 
     supportNeedsLegacyProfileService.addLegacySupportNeeds()
-    
-    verify(prisonerSupportNeedRepository).saveAll(listOf(
-      PrisonerSupportNeedEntity(prisonerId = 1, supportNeed = SupportNeedEntity(id = 101, pathway = Pathway.ACCOMMODATION, section = "Legacy support need", title = "Accommodation", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
-      PrisonerSupportNeedEntity(prisonerId = 1, supportNeed = SupportNeedEntity(id = 102, pathway = Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, section = "Legacy support need", title = "Attitudes, thinking and behaviour", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
-      PrisonerSupportNeedEntity(prisonerId = 1, supportNeed = SupportNeedEntity(id = 103, pathway = Pathway.HEALTH, section = "Legacy support need", title = "Health", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
-    ))
 
-    verify(prisonerSupportNeedRepository).saveAll(listOf(
-      PrisonerSupportNeedEntity(prisonerId = 2, supportNeed = SupportNeedEntity(id = 101, pathway = Pathway.ACCOMMODATION, section = "Legacy support need", title = "Accommodation", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
-      PrisonerSupportNeedEntity(prisonerId = 2, supportNeed = SupportNeedEntity(id = 102, pathway = Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, section = "Legacy support need", title = "Attitudes, thinking and behaviour", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
-      PrisonerSupportNeedEntity(prisonerId = 2, supportNeed = SupportNeedEntity(id = 103, pathway = Pathway.HEALTH, section = "Legacy support need", title = "Health", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
-    ))
+    verify(prisonerSupportNeedRepository).saveAll(
+      listOf(
+        PrisonerSupportNeedEntity(prisonerId = 1, supportNeed = SupportNeedEntity(id = 101, pathway = Pathway.ACCOMMODATION, section = "Legacy support need", title = "Accommodation", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
+        PrisonerSupportNeedEntity(prisonerId = 1, supportNeed = SupportNeedEntity(id = 102, pathway = Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, section = "Legacy support need", title = "Attitudes, thinking and behaviour", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
+        PrisonerSupportNeedEntity(prisonerId = 1, supportNeed = SupportNeedEntity(id = 103, pathway = Pathway.HEALTH, section = "Legacy support need", title = "Health", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
+      ),
+    )
+
+    verify(prisonerSupportNeedRepository).saveAll(
+      listOf(
+        PrisonerSupportNeedEntity(prisonerId = 2, supportNeed = SupportNeedEntity(id = 101, pathway = Pathway.ACCOMMODATION, section = "Legacy support need", title = "Accommodation", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
+        PrisonerSupportNeedEntity(prisonerId = 2, supportNeed = SupportNeedEntity(id = 102, pathway = Pathway.ATTITUDES_THINKING_AND_BEHAVIOUR, section = "Legacy support need", title = "Attitudes, thinking and behaviour", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
+        PrisonerSupportNeedEntity(prisonerId = 2, supportNeed = SupportNeedEntity(id = 103, pathway = Pathway.HEALTH, section = "Legacy support need", title = "Health", hidden = true, excludeFromCount = false, allowOtherDetail = true, createdDate = LocalDateTime.parse("2025-02-28T12:00"), deleted = false, deletedDate = null), otherDetail = null, createdBy = "System User", createdDate = fakeNow, deleted = false, deletedDate = null, latestUpdateId = null),
+      ),
+    )
 
     unmockkAll()
   }

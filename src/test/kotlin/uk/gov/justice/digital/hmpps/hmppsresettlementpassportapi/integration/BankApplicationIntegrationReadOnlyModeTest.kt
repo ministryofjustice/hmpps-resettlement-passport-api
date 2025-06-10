31e2190e989f46d46afa081e9ade6495c92ade16
Mary Ford <201111422+Mary-ford@users.noreply.github.com>
2025-06-10 13:51:47 +0100

    Split out separate tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/BankApplicationIntegrationReadOnlyModeTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportapi/integration/BankApplicationIntegrationReadOnlyModeTest.kt
@@ -24,29 +24,35 @@ class BankApplicationIntegrationReadOnlyModeTest : IntegrationTestBase() {
 
   @Test
   @Sql("classpath:testdata/sql/seed-bank-application.sql")
-  fun `Create, update and delete bank application - forbidden`() {
-    val nomsId = "123"
-
+  fun `Create bank application - forbidden`() {
     webTestClient.post()
-      .uri("/resettlement-passport/prisoner/$nomsId/bankapplication")
+      .uri("/resettlement-passport/prisoner/123/bankapplication")
       .bodyValue(
         BankApplication(applicationSubmittedDate = fakeNow, bankName = "Lloyds"),
       )
       .headers(setAuthorisation(roles = listOf("ROLE_RESETTLEMENT_PASSPORT_EDIT")))
       .exchange()
       .expectStatus().isForbidden
+  }
 
+  @Test
+  @Sql("classpath:testdata/sql/seed-bank-application.sql")
+  fun `Update bank application - forbidden`() {
     webTestClient.patch()
-      .uri("/resettlement-passport/prisoner/$nomsId/bankapplication/1")
+      .uri("/resettlement-passport/prisoner/123/bankapplication/1")
       .bodyValue(
         BankApplication(resubmissionDate = fakeNow, bankResponseDate = fakeNow, status = "Account opened"),
       )
       .headers(setAuthorisation(roles = listOf("ROLE_RESETTLEMENT_PASSPORT_EDIT")))
       .exchange()
       .expectStatus().isForbidden
+  }
 
+  @Test
+  @Sql("classpath:testdata/sql/seed-bank-application.sql")
+  fun `Delete bank application - forbidden`() {
     webTestClient.delete()
-      .uri("/resettlement-passport/prisoner/$nomsId/bankapplication/1")
+      .uri("/resettlement-passport/prisoner/123/bankapplication/1")
       .headers(setAuthorisation(roles = listOf("ROLE_RESETTLEMENT_PASSPORT_EDIT")))
       .exchange()
       .expectStatus().isForbidden

9d92c2b000f9f4093c8764970c63fb50bcceabd4
Mary Ford <201111422+Mary-ford@users.noreply.github.com>
2025-06-13 12:57:31 +0100

    test plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+import org.gradle.kotlin.dsl.maven
+import org.gradle.kotlin.dsl.mavenCentral
+
 rootProject.name = "hmpps-resettlement-passport-api"
+
+pluginManagement {
+  repositories {
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("/Users/mary.ford/Documents/dev/gitleaks-kotlin-plugin/plugins.local") }
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
